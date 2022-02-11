51ba2016714f9dca801fec943e23e9a63b4cf0c7
Nigel <nigel.bmlt@gmail.com>
2022-02-11 20:11:02 +1100

    update settings

--- meeting-admin-workflow.php
+++ meeting-admin-workflow.php
@@ -27,8 +27,17 @@ function enqueue_form_deps() {
 
 add_action( 'wp_enqueue_scripts', 'enqueue_form_deps' );
 
+/* ------------------------------------------------------------------------ *
+ * Setting Registration
+ * ------------------------------------------------------------------------ */
+ 
+/**
+ * Initializes the theme options page by registering the Sections,
+ * Fields, and Settings.
+ *
+ * This function is registered with the 'admin_init' hook.
+ */
 add_action('admin_init', 'sandbox_initialize_theme_options');
-
 function sandbox_initialize_theme_options() {
  
     // First, we register a section. This is necessary since all future options must belong to one.
@@ -38,6 +47,19 @@ function sandbox_initialize_theme_options() {
         'sandbox_general_options_callback', // Callback used to render the description of the section
         'general'                           // Page on which to add this section of options
     );
+     
+    // Next, we will introduce the fields for toggling the visibility of content elements.
+    add_settings_field( 
+        'show_header',                      // ID used to identify the field throughout the theme
+        'Header',                           // The label to the left of the option interface element
+        'sandbox_toggle_header_callback',   // The name of the function responsible for rendering the option interface
+        'general',                          // The page on which this option will be displayed
+        'general_settings_section',         // The name of the section to which this field belongs
+        array(                              // The array of arguments to pass to the callback. In this case, just a description.
+            'Activate this setting to display the header.'
+        )
+    );
+     
 } // end sandbox_initialize_theme_options
  
 /* ------------------------------------------------------------------------ *
@@ -53,5 +75,27 @@ function sandbox_initialize_theme_options() {
 function sandbox_general_options_callback() {
     echo '<p>Select which areas of content you wish to display.</p>';
 } // end sandbox_general_options_callback
-  
+ 
+/* ------------------------------------------------------------------------ *
+ * Field Callbacks
+ * ------------------------------------------------------------------------ */
+ 
+/**
+ * This function renders the interface elements for toggling the visibility of the header element.
+ * 
+ * It accepts an array of arguments and expects the first element in the array to be the description
+ * to be displayed next to the checkbox.
+ */
+function sandbox_toggle_header_callback($args) {
+     
+    // Note the ID and the name attribute of the element match that of the ID in the call to add_settings_field
+    $html = '<input type="checkbox" id="show_header" name="show_header" value="1" ' . checked(1, get_option('show_header'), false) . '/>'; 
+     
+    // Here, we will take the first argument of the array and add it to a label next to the checkbox
+    $html .= '<label for="show_header"> '  . $args[0] . '</label>'; 
+     
+    echo $html;
+     
+} // end sandbox_toggle_header_callback
+ 
 ?>
