4adf20904d75fc010fee59012e618eb983442cb8
Nigel <nigel.bmlt@gmail.com>
2022-02-12 13:36:54 +1100

    move more stuff around

--- meeting-admin-workflow.php
+++ meeting-admin-workflow.php
@@ -49,11 +49,6 @@ function bmaw_initialise_options()
     //     'show_header'
     // );
     add_submenu_page('options-general.php','BMAW','BMAW','manage_options','bmaw_settings_page','display_admin_options_page');
-}
-
-function bmaw_initialise_settings()
-{
-    dbg('in initialize settings');
 
     add_settings_section(
         'list_service_areas_section',         // ID used to identify this section and with which to register options
@@ -76,6 +71,33 @@ function bmaw_initialise_settings()
         'BMAW',
         'list_service_areas_field'
     );
+}
+
+function bmaw_initialise_settings()
+{
+    dbg('in initialize settings');
+
+    // add_settings_section(
+    //     'list_service_areas_section',         // ID used to identify this section and with which to register options
+    //     'Service Areas',                  // Title to be displayed on the administration page
+    //     'service_areas_section_callback', // Callback used to render the description of the section
+    //     'bmaw_settings_page'                           // Page on which to add this section of options
+    // );
+
+    // add_settings_field(
+    //     'list_service_areas_field',                      // ID used to identify the field throughout the theme
+    //     'Service Areas',                           // The label to the left of the option interface element
+    //     'list_service_areas_callback',   // The name of the function responsible for rendering the option interface
+    //     'bmaw_settings_page',                          // The page on which this option will be displayed
+    //     'list_service_areas_section',         // The name of the section to which this field belongs
+    //     array(                              // The array of arguments to pass to the callback. In this case, just a description.
+    //         'Activate this setting to display the header.'
+    //     )
+    // );
+    // register_setting(
+    //     'BMAW',
+    //     'list_service_areas_field'
+    // );
 }
 
 function list_service_areas_callback($args)
