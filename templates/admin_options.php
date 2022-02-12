24535e76e12e89ef88cbfbd43d0b9ea1a3632816
Nigel <nigel.bmlt@gmail.com>
2022-02-12 13:42:10 +1100

    update admin template

--- templates/admin_options.php
+++ templates/admin_options.php
@@ -1,9 +1,16 @@
 <?php
 
-// if ( !current_user_can( 'manage_options' ) ) {
-//     exit;
-// }
+if ( !current_user_can( 'manage_options' ) ) {
+    exit;
+}
 
 ?>
 
 <h1>hello</h1>
+
+<?php
+
+settings_fields('list_service_areas_field');
+do_settings_sections('list_service_areas_section');
+
+?>
