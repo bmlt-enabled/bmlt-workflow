319863995520a459b332fd5c5fa3852383b01337
Nigel <nigel.bmlt@gmail.com>
2022-02-13 00:48:03 +1100

    try checkbox click

--- templates/admin_options.php
+++ templates/admin_options.php
@@ -1,7 +1,21 @@
 <?php
-echo '<div class="wrap">
+echo <<<END
+<div class="wrap">
+<script>
+$(document).ready(function() {
+
+    $('#committeetable tr').click(function() {
+        var cb = $(this).find("input:checkbox")
+        if(cb) {
+            cb..prop( "checked", true )
+        }
+    });
+
+});
+</script>
 <h1>BMLT Meeting Admin Workflow Settings</h1>
 <form method="post" action="options.php">';
+END;
         
     settings_fields( 'bmaw-settings-group' ); // settings group name
     do_settings_sections( 'bmaw-settings' ); // just a page slug
