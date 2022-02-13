24872e73497af4bfd4e25999d814861a6a3102e8
Nigel <nigel.bmlt@gmail.com>
2022-02-13 21:46:37 +1100

    fix the add remove

--- templates/admin_options.php
+++ templates/admin_options.php
@@ -3,27 +3,26 @@ echo <<<END
 <div class="wrap">
 <script>
 jQuery(document).ready(function($) {
-
-    jQuery('#bmaw-service-committee-table tr td:nth-child(4)').click(function() { 
+    $("#bmaw-service-committee-table tbody").on("click", "tr td:nth-child(4)", function(event){
         var rowCount = $('#bmaw-service-committee-table tr').length-2;
         var clicked = $(this).closest('tr').index();
         console.log("table length "+rowCount+" row clicked "+clicked);
         if (clicked == rowCount)
         {
             console.log("add row")
-            $('#bmaw-service-committee-table > tbody:last-child').prev().append(`<tr>
+            $('#bmaw-service-committee-table > tbody > tr').eq(rowCount-1).after(`<tr>
             <td><input type="text" name="bmaw_service_committee_option_array[' . rowCount . '][name]" value=""/></td>
             <td><input type="text" name="bmaw_service_committee_option_array[' . rowCount . '][e1]" value=""/></td>
             <td><input type="text" name="bmaw_service_committee_option_array[' . rowCount . '][e2]" value=""/></td>
-            <td><span id="bmaw-service-committee-new-row" class="dashicons dashicons-insert"></span></td>
+            <td><span id="bmaw-service-committee-new-row" class="dashicons dashicons-remove"></span></td>
             </tr>`);
         }
         else
         {
+        $('#bmaw-service-committee-table > tbody > tr').eq(clicked).remove()
             console.log("delete row")
         }
     });
-
 });
 </script>
 <h1>BMLT Meeting Admin Workflow Settings</h1>
