47f5b4f5f2c6334a27cad569efd557bea6adcf8d
Nigel <nigel.bmlt@gmail.com>
2022-02-19 11:29:40 +1100

    publish

--- admin/admin_options.php
+++ admin/admin_options.php
@@ -4,55 +4,14 @@ $arr = get_option('bmaw_service_committee_option_array');
 $js_array = json_encode($arr);
 
 echo '<div class="wrap"><script>';
-echo 'jQuery(document).ready(function($) {';
-echo "var bmaw_service_form_array = ". $js_array . ";\n";
-echo <<<END
+echo "var bmaw_service_form_array = ". $js_array . ";</script>";
+echo '<script src="'.plugin_dir_url(__FILE__).'js/admin_page.js"</script>';
 
-$("#bmaw-bmlt-test-server").on("click", function(event) {
-console.log("clicked");
-}
+echo '<form method="post" action="options.php">';
 
-$("#bmaw-service-committee-table tbody").on("click", "tr td:nth-child(4)", function (event) {
-    var rowCount = $("#bmaw-service-committee-table tr").length - 2;
-    var clicked = $(this).closest("tr").index();
-    console.log("table length " + rowCount + " row clicked " + clicked);
-    if (clicked == rowCount) {
-      console.log("add row");
-      $("#bmaw-service-committee-table > tbody > tr")
-        .eq(rowCount - 1)
-        .after(
-          '<tr><td><input type="text" name="bmaw_service_committee_option_array[' +
-            rowCount +
-            '][name]" value=""/></td><td><input type="text" name="bmaw_service_committee_option_array[' +
-            rowCount +
-            '][e1]" value=""/></td><td><input type="text" name="bmaw_service_committee_option_array[' +
-            rowCount +
-            '][e2]" value=""/></td><td><span id="bmaw-service-committee-new-row" class="dashicons dashicons-remove"></span></td></tr>'
-        );
-    } else if (rowCount != 1) {
-      $("#bmaw-service-committee-table > tbody > tr").eq(clicked).remove();
-      for (var i = clicked; i < rowCount; i++) {
-        console.log("i = " + i + " rowCount = " + rowCount);
-        var row = $("#bmaw-service-committee-table > tbody > tr").eq(i);
-        row.find("td input").each(function () {
-          var a = $(this).attr("name");
-          console.log("a = " + a);
-          var b = a.replace(/\[[0-9]\]/i, "[" + i + "]");
-          console.log("b = " + b);
-          $(this).attr("name", b);
-        });
-      }
-    }
-  });
-});
-</script>
-<h1>BMLT Meeting Admin Workflow Settings</h1>
-<form method="post" action="options.php">
-END;
-        
-    settings_fields( 'bmaw-settings-group' );
-    do_settings_sections( 'bmaw-settings' );
+settings_fields( 'bmaw-settings-group' );
+do_settings_sections( 'bmaw-settings' );
 
-    submit_button();
+submit_button();
 
 echo '</form></div>';
