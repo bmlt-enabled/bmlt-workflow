be310516719c93d4199d56a038bd00fc5c00e625
Nigel <nigel.bmlt@gmail.com>
2022-02-19 12:06:45 +1100

    update

--- admin/admin_options.php
+++ admin/admin_options.php
@@ -7,7 +7,7 @@ echo '<div class="wrap"><script>';
 echo "var bmaw_service_form_array = ". $js_array . ";</script>";
 // echo '<script src="'.THIS_PLUGIN_URL.'js/admin_page.js"</script>';
 // echo '<script src="http://54.153.167.239/flop/wp-content/plugins/meeting-admin-workflow/js/admin_page.js?ver=1645231395"></script>';
-echo '<script src="'.esc_url( plugins_url( 'js/admin_page.js', dirname(__FILE__))).'"</script>';
+echo '<script src="'.esc_url( plugins_url( 'js/admin_page.js', dirname(__FILE__))).'"></script>';
 echo '<form method="post" action="options.php">';
 
 settings_fields( 'bmaw-settings-group' );
