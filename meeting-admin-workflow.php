becd9f181556634caf3eb2755e9eda98c4513792
Nigel <nigel.bmlt@gmail.com>
2022-02-20 15:32:02 +1100

    debug

--- meeting-admin-workflow.php
+++ meeting-admin-workflow.php
@@ -471,33 +471,33 @@ function meeting_update_form_response()
                 wp_die(("No valid service committee found."));
             }
 
-            if (($reason == "reason_change") || ($reason == 'reason_close')) {
-                dbg("getting http from server");
-                $meeting_id = $_POST['id_bigint'];
-                $bmaw_bmlt_server_address = get_option('bmaw_bmlt_server_address');
-                $url = $bmaw_bmlt_server_address . "/client_interface/json/?switcher=GetSearchResults&meeting_key=id_bigint&meeting_key_value=".$meeting_id."lang_enum=en&data_field_key=location_postal_code_1,duration_time,start_time,time_zone,weekday_tinyint,service_body_bigint,longitude,latitude,location_province,location_municipality,location_street,location_info,location_neighborhood,formats,format_shared_id_list,comments,location_sub_province,worldid_mixed,root_server_uri,id_bigint,venue_type,meeting_name,location_text,virtual_meeting_additional_info,contact_name_1,contact_phone_1,contact_email_1,contact_name_2,contact_phone_2,contact_email_2&&recursive=1&sort_keys=start_time";
+            if (isset($_POST['update_reason'])) {
+                if (($reason == "reason_change") || ($reason == 'reason_close')) {
+                    dbg("getting http from server");
+                    $meeting_id = $_POST['id_bigint'];
+                    $bmaw_bmlt_server_address = get_option('bmaw_bmlt_server_address');
+                    $url = $bmaw_bmlt_server_address . "/client_interface/json/?switcher=GetSearchResults&meeting_key=id_bigint&meeting_key_value=" . $meeting_id . "&lang_enum=en&data_field_key=location_postal_code_1,duration_time,start_time,time_zone,weekday_tinyint,service_body_bigint,longitude,latitude,location_province,location_municipality,location_street,location_info,location_neighborhood,formats,format_shared_id_list,comments,location_sub_province,worldid_mixed,root_server_uri,id_bigint,venue_type,meeting_name,location_text,virtual_meeting_additional_info,contact_name_1,contact_phone_1,contact_email_1,contact_name_2,contact_phone_2,contact_email_2&&recursive=1&sort_keys=start_time";
 
-                dbg("url = ".$url);
-                $curl = curl_init($url);
-                curl_setopt($curl, CURLOPT_URL, $url);
-                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                    dbg("url = " . $url);
+                    $curl = curl_init($url);
+                    curl_setopt($curl, CURLOPT_URL, $url);
+                    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-                $headers = array(
-                    "Accept: */*",
-                );
-                curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+                    $headers = array(
+                        "Accept: */*",
+                    );
+                    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
-                $resp = curl_exec($curl);
-                if(!$resp)
-                {
-                    dbg("curl failed");
+                    $resp = curl_exec($curl);
+                    if (!$resp) {
+                        dbg("curl failed");
+                    } else {
+                        dbg(print_r($resp));
+                    }
+                    curl_close($curl);
                 }
-                else
-                {
-                    dbg(print_r($resp));    
-                }
-                curl_close($curl);
-
+            } else {
+                wp_die("meeting id not set");
             }
 
             $from_address = get_option('bmaw_email_from_address');
