78f63f58d9e038ad7c321ca6f293859eeac0fbe6
Nigel <nigel.bmlt@gmail.com>
2022-02-24 20:55:35 +1100

    update

--- admin/admin_options.php
+++ admin/admin_options.php
@@ -2,22 +2,15 @@
 
 $arr = get_option('bmaw_service_committee_option_array');
 $js_array = json_encode($arr);
-$test_result = get_option('bmaw_test_successful','failed');
 $bmaw_close_meeting_template_default = htmlentities(file_get_contents(BMAW_PLUGIN_DIR . 'templates/default_close_meeting_email_template.html'));
 $bmawt_other_meeting_template_defaul = htmlentities(file_get_contents(BMAW_PLUGIN_DIR . 'templates/default_other_meeting_email_template.html'));
 $bmaw_new_meeting_template_default = htmlentities(file_get_contents(BMAW_PLUGIN_DIR . 'templates/default_new_meeting_email_template.html'));
 $bmaw_existing_meeting_template_default = htmlentities(file_get_contents(BMAW_PLUGIN_DIR . 'templates/default_existing_meeting_email_template.html'));
 
-if($test_result!='succeeded')
-{
-    $test_result = 'failed';
-}
-
 echo '<div class="wrap">';
 echo '<script src="'.esc_url( plugins_url( 'js/clipboard.js', dirname(__FILE__))).'"></script>';
 echo '<script>var clipboard = new ClipboardJS(".clipboard-button");</script>';
 echo '<script>var bmaw_service_form_array = '. $js_array . '</script>';
-echo '<script>var bmaw_test_successful = "'. $test_result .'"</script>';
 
 echo '<div style="position:absolute; top:0; left:-500px;"><textarea rows="1" cols="2" id="bmaw_new_meeting_template_default">'.$bmaw_new_meeting_template_default.'</textarea></div>';
 echo '<div style="position:absolute; top:0; left:-500px;"><textarea rows="1" cols="2" id="bmaw_existing_meeting_template_default">'.$bmaw_existing_meeting_template_default.'</textarea></div>';
