47e95349c8181ca01e192986485e9200985fbc89
Nigel <nigel.bmlt@gmail.com>
2022-02-24 20:48:12 +1100

    update

--- meeting-admin-workflow.php
+++ meeting-admin-workflow.php
@@ -296,7 +296,7 @@ function bmaw_register_setting()
 function bmaw_bmlt_server_address_html()
 {
     $bmaw_bmlt_server_address = get_option('bmaw_bmlt_server_address');
-    $bmaw_bmlt_test_status = update_option('bmaw_bmlt_test_status',"failure");
+    $bmaw_bmlt_test_status = get_option('bmaw_bmlt_test_status',"failure");
     echo <<<END
     <div class="bmaw_info_text">
     <br>Your BMLT server address, used to populate the meeting list for meeting changes and closures. For example: <code>https://na.org.au/main_server/</code>
