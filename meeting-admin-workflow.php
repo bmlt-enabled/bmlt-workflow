a697ba49741f8d1fc600292d3257cfaf71390a8e
Nigel <nigel.bmlt@gmail.com>
2022-02-26 22:14:26 +1100

    bugfix

--- meeting-admin-workflow.php
+++ meeting-admin-workflow.php
@@ -11,17 +11,10 @@
 
 define('BMAW_PLUGIN_DIR', plugin_dir_path(__FILE__));
 
-function dbg($logmsg)
-{
-    $log = plugin_dir_path(__FILE__) . 'debug.log';
-    error_log($logmsg . PHP_EOL, 3, $log);
-}
-
 include 'form handlers/meeting-update-form-handler.php';
 
 function meeting_update_form($atts = [], $content = null, $tag = '')
 {
-    // dbg("atts = ".$atts);
     $parsed_atts = shortcode_atts(
         array(
             'service-areas' => '1',
@@ -92,19 +85,16 @@ add_filter('plugin_action_links', 'add_plugin_link', 10, 2);
 
 function array_sanitize_callback($args)
 {
-    // dbg('array sanitise called' . $args);
     return $args;
 }
 
 function editor_sanitize_callback($args)
 {
-    // dbg("called editor sanitize");
     return $args;
 }
 
 function string_sanitize_callback($args)
 {
-    // dbg("called string sanitize");
     return $args;
 }
 
@@ -502,7 +492,6 @@ function bmaw_close_meeting_template_html()
 function bmaw_service_committee_table_html()
 {
 
-    // dbg("printing the text field");
     $arr = get_option('bmaw_service_committee_option_array');
 
     echo <<<END
