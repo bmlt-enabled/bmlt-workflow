4c681256467f726182b95e1e59bc67d7db816434
Nigel <nigel.bmlt@gmail.com>
2022-02-28 14:11:42 +1100

    matcher tests

--- js/meeting_update.js
+++ js/meeting_update.js
@@ -275,11 +275,38 @@ jQuery(document).ready(function ($) {
         mtext[i] = { text: str, id: i };
       }
 
+      function matchCustom(params, data) {
+        // If there are no search terms, return all of the data
+        if ($.trim(params.term) === '') {
+          return data;
+        }
+    
+        // Do not display the item if there is no 'text' property
+        if (typeof data.text === 'undefined') {
+          return null;
+        }
+    
+        // `params.term` should be the term that is used for searching
+        // `data.text` is the text that is displayed for the data object
+        if (data.text.indexOf(params.term) > -1) {
+          var modifiedData = $.extend({}, data, true);
+          modifiedData.text += ' (matched)';
+    
+          // You can return modified objects from here
+          // This includes matching the `children` how you want in nested data sets
+          return modifiedData;
+        }
+    
+        // Return `null` if the term should not be displayed
+        return null;
+    }
+
       $(".select2-ajax").select2({
         data: mtext,
         placeholder: "Select a meeting",
         allowClear: true,
         dropdownAutoWidth: true,
+        matcher: matchCustom
       });
 
       $("#meeting-searcher").one("select2:open", function (e) {
