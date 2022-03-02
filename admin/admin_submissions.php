747042891d2885a7f2c54e47fb1e7a58748f1d57
Nigel <nigel.bmlt@gmail.com>
2022-03-02 21:30:47 +1030

    try sample table

--- admin/admin_submissions.php
+++ admin/admin_submissions.php
@@ -1,164 +1,488 @@
+
 <?php
-
-if (!defined('ABSPATH')) exit; // die if being called directly
-
-$exampleListTable = new bmaw_meeting_submissions_page();
-
-error_log("created new bmaw_meeting_submissions_page");
-$exampleListTable->prepare_items();
-?>
-<div class="wrap">
-    <div id="icon-users" class="icon32"></div>
-    <h2>Meeting Submissions</h2>
-    <?php $exampleListTable->display(); ?>
-</div>
-<?php
-
-if (!class_exists('WP_List_Table')) {
-    require_once(ABSPATH . 'wp-admin/includes/class-wp-list-table.php');
-}
-
-class bmaw_meeting_submissions_page extends WP_List_Table
-{
-    public function prepare_items()
-    {
-        $columns = $this->get_columns();
-        $hidden = $this->get_hidden_columns();
-        $sortable = $this->get_sortable_columns();
-
-        $data = $this->table_data();
-        usort($data, array(&$this, 'sort_data'));
-
-        $perPage = 10;
-        $currentPage = $this->get_pagenum();
-        $totalItems = count($data);
-
-        $this->set_pagination_args(array(
-            'total_items' => $totalItems,
-            'per_page'    => $perPage
-        ));
-
-        $data = array_slice($data, (($currentPage - 1) * $perPage), $perPage);
-
-        $this->_column_headers = array($columns, $hidden, $sortable);
-        $this->items = $data;
-    }
-    // id mediumint(9) NOT NULL AUTO_INCREMENT,
-    // submission_time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-    // change_time datetime DEFAULT '0000-00-00 00:00:00',
-    // changed_by varchar(10),
-    // change_made varchar(10),
-    // submitter_name tinytext NOT NULL,
-    // submission_type tinytext NOT NULL,
-    // submitter_email varchar(320) NOT NULL,
-
-    public function get_columns()
-    {
-        $columns = array(
-            'id'          => 'ID',
-            'submitter_name'       => 'Submitter Name',
-            'submitter_email' => 'Submitter Email',
-            'submission_type'        => 'Change Type',
-            'submission_time'    => 'Submission Time',
-            'change_time' => 'Change Time',
-            'changed_by' => 'Changed By',
-            'change_made' => 'Change Made'
-        );
-
-        return $columns;
-    }
-
-    public function get_hidden_columns()
-    {
-        return array();
-    }
-
-    public function get_sortable_columns()
-    {
-        return array('submitter_name' => array('submitter_name', false));
-    }
+if ( ! class_exists( 'WP_List_Table' ) )
+	require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
 
 
-    public function column_id($item)
-    {
-        $edit_link = admin_url('post.php?action=edit&amp;post=' .  $item['id']);
-        $view_link = get_permalink($item['id']);
-        $output    = '';
+$myajaxurl = "http://54.153.167.239/flop/wp-json/bmaw-submission/v1/submission";
 
-        // Title.
-        $output .= '<strong><a href="' . esc_url($edit_link) . '" class="row-title">' . esc_html($item['id']) . '</a></strong>';
+/**
+ *
+ * As Charlie suggested in its plugin https://github.com/Askelon/Custom-AJAX-List-Table-Example
+ * it's better to set the error_reporting in order to hiding notices to avoid AJAX errors
+ *
+ */
 
-        // Get actions.
-        $actions = array(
-            '1'   => '<a target="_blank" href="' . esc_url($edit_link) . '">' . esc_html__('Approve', 'my_plugin') . '</a>',
-            '2'   => '<a target="_blank" href="' . esc_url($view_link) . '">' . esc_html__('Reject', 'my_plugin') . '</a>',
-            '3'   => '<a target="_blank" href="' . esc_url($view_link) . '">' . esc_html__('View Detail', 'my_plugin') . '</a>',
-        );
+error_reporting( ~E_NOTICE );
 
-        $row_actions = array();
+class My_List_Table extends WP_List_Table {
 
-        foreach ($actions as $action => $link) {
-            $row_actions[] = '<span class="' . esc_attr($action) . '">' . $link . '</span>';
-        }
 
-        $output .= '<div class="row-actions">' . implode(' | ', $row_actions) . '</div>';
+	/**
+	 *
+	 * @Override of constructor
+	 * Constructor take 3 parameters:
+	 * singular : name of an element in the List Table
+	 * plural : name of all of the elements in the List Table
+	 * ajax : if List Table supports AJAX set to true
+	 *
+	 */
 
-        return $output;
-    }
+	function __construct() {
 
-    public function column_default($item, $column_name)
-    {
-        switch ($column_name) {
-                // case 'id':
-            case 'submitter_name':
-            case 'submitter_email':
-            case 'submission_type':
-            case 'submission_date_time':
-            case 'change_time':
-            case 'changed_by':
-            case 'change_made':
-            case 'submission_time':
-                return $item[$column_name];
+		parent::__construct(
+			array(
+				'singular'  => '60s hit',
+				'plural'    => '60s hits',
+				'ajax'      => true
+			)
+		);
 
-            default:
-                return print_r($item, true);
-        }
-    }
+	}
 
-    private function table_data()
-    {
-        global $wpdb;
-        global $bmaw_submissions_table_name;
+	/**
+	 * @return array
+	 *
+	 * The array is associative :
+	 * keys are slug columns
+	 * values are description columns
+	 *
+	 */
+
+	function get_columns() {
+
+		$columns = array(
+			'id'      => 'ID',
+			'title'   => 'Title',
+			'artist'  => 'Artist',
+			'year'    => 'Year'
+		);
+		return $columns;
+
+	}
+
+	/**
+	 * @param $item
+	 * @param $column_name
+	 *
+	 * @return mixed
+	 *
+	 * Method column_default let at your choice the rendering of everyone of column
+	 *
+	 */
+
+	function column_default( $item, $column_name ) {
+		switch( $column_name ) {
+			case 'id':
+			case 'title':
+			case 'artist':
+			case 'year':
+				return $item[ $column_name ];
+			default:
+				return print_r( $item, true );
+		}
+	}
+
+	/**
+	 * @var array
+	 *
+	 * Array contains slug columns that you want hidden
+	 *
+	 */
+
+	private $hidden_columns = array(
+		'id'
+	);
+
+	/**
+	 * @return array
+	 *
+	 * The array is associative :
+	 * keys are slug columns
+	 * values are array of slug and a boolean that indicates if is sorted yet
+	 *
+	 */
+
+	function get_sortable_columns() {
+
+		return $sortable_columns = array(
+			'title'	 	=> array( 'title', false ),
+			'artist'	=> array( 'artist', false ),
+			'year'   	=> array( 'year', false )
+		);
+	}
+
+	/**
+	 * @Override of prepare_items method
+	 *
+	 */
+
+	function prepare_items() {
+
+		/**
+		 * How many records for page do you want to show?
+		 */
+		$per_page = 5;
+
+		/**
+		 * Define of column_headers. It's an array that contains:
+		 * columns of List Table
+		 * hiddens columns of table
+		 * sortable columns of table
+		 * optionally primary column of table
+		 */
+		$columns  = $this->get_columns();
+		$hidden   = $this->hidden_columns;
+		$sortable = $this->get_sortable_columns();
+		$this->_column_headers = array($columns, $hidden, $sortable);
+
+		/**
+		 * Following lines are only a sample with a static array
+		 * in a real situation you can get data
+		 * from a REST architecture or from database (using $wpdb)
+		 */
+		// $data = $this->sample_data;
+        $data = array();
         
-        $result = $wpdb->get_results('SELECT * FROM '.$bmaw_submissions_table_name, ARRAY_A);
+		function usort_reorder( $a, $b ) {
 
-        return $result;
-    }
+			$orderby = ( ! empty( $_REQUEST['orderby'] ) ) ? $_REQUEST['orderby'] : 'title';
+			$order = ( ! empty( $_REQUEST['order'] ) ) ? $_REQUEST['order'] : 'asc';
+			$result = strcmp( $a[ $orderby ], $b[ $orderby ] );
+			return ( 'asc' === $order ) ? $result : -$result;
+		}
+		usort( $data, 'usort_reorder' );
 
-    private function sort_data($a, $b)
-    {
-        // Set defaults
-        $orderby = 'submitter_name';
-        $order = 'asc';
+		/**
+		 * Get current page calling get_pagenum method
+		 */
+		$current_page = $this->get_pagenum();
 
-        // If orderby is set, use this as the sort column
-        if (!empty($_GET['orderby'])) {
-            $orderby = $_GET['orderby'];
-        }
+		$total_items = count($data);
 
-        // If order is set use this as the order
-        if (!empty($_GET['order'])) {
-            $order = $_GET['order'];
-        }
+		$data = array_slice($data,(($current_page-1)*$per_page),$per_page);
 
+		$this->items = $data;
 
-        $result = strcmp($a[$orderby], $b[$orderby]);
+		/**
+		 * Call to _set_pagination_args method for informations about
+		 * total items, items for page, total pages and ordering
+		 */
+		$this->set_pagination_args(
+			array(
 
-        if ($order === 'asc') {
-            return $result;
-        }
+				'total_items'	=> $total_items,
+				'per_page'	    => $per_page,
+				'total_pages'	=> ceil( $total_items / $per_page ),
+				'orderby'	    => ! empty( $_REQUEST['orderby'] ) && '' != $_REQUEST['orderby'] ? $_REQUEST['orderby'] : 'title',
+				'order'		    => ! empty( $_REQUEST['order'] ) && '' != $_REQUEST['order'] ? $_REQUEST['order'] : 'asc'
+			)
+		);
+	}
 
-        return -$result;
-    }
+	/**
+	 * @Override of display method
+	 */
+
+	function display() {
+
+		/**
+		 * Adds a nonce field
+		 */
+		wp_nonce_field( 'ajax-custom-list-nonce', '_ajax_custom_list_nonce' );
+
+		/**
+		 * Adds field order and orderby
+		 */
+		echo '<input type="hidden" id="order" name="order" value="' . $this->_pagination_args['order'] . '" />';
+		echo '<input type="hidden" id="orderby" name="orderby" value="' . $this->_pagination_args['orderby'] . '" />';
+
+		parent::display();
+	}
+
+	/**
+	 * @Override ajax_response method
+	 */
+
+	function ajax_response() {
+
+		check_ajax_referer( 'ajax-custom-list-nonce', '_ajax_custom_list_nonce' );
+
+		$this->prepare_items();
+
+		extract( $this->_args );
+		extract( $this->_pagination_args, EXTR_SKIP );
+
+		ob_start();
+		if ( ! empty( $_REQUEST['no_placeholder'] ) )
+			$this->display_rows();
+		else
+			$this->display_rows_or_placeholder();
+		$rows = ob_get_clean();
+
+		ob_start();
+		$this->print_column_headers();
+		$headers = ob_get_clean();
+
+		ob_start();
+		$this->pagination('top');
+		$pagination_top = ob_get_clean();
+
+		ob_start();
+		$this->pagination('bottom');
+		$pagination_bottom = ob_get_clean();
+
+		$response = array( 'rows' => $rows );
+		$response['pagination']['top'] = $pagination_top;
+		$response['pagination']['bottom'] = $pagination_bottom;
+		$response['column_headers'] = $headers;
+
+		if ( isset( $total_items ) )
+			$response['total_items_i18n'] = sprintf( _n( '1 item', '%s items', $total_items ), number_format_i18n( $total_items ) );
+
+		if ( isset( $total_pages ) ) {
+			$response['total_pages'] = $total_pages;
+			$response['total_pages_i18n'] = number_format_i18n( $total_pages );
+		}
+
+		die( json_encode( $response ) );
+	}
+
 }
+
+function display_bmaw_admin_submissions_page() {
+
 ?>
+
+	<div class="wrap">
+
+		<h2>WP List Table Ajax Sample</h2>
+
+		<form id="email-sent-list" method="get">
+
+			<input type="hidden" name="page" value="<?php echo $_REQUEST['page']; ?>" />
+			<input type="hidden" name="order" value="<?php echo $_REQUEST['order']; ?>" />
+			<input type="hidden" name="orderby" value="<?php echo $_REQUEST['orderby']; ?>" />
+
+			<div id="ts-history-table" style="">
+				<?php
+				wp_nonce_field( 'ajax-custom-list-nonce', '_ajax_custom_list_nonce' );
+				?>
+			</div>
+
+		</form>
+
+	</div>
+
+<?php
+
+}
+
+/**
+ * Action wp_ajax for fetching ajax_response
+ */
+
+function _ajax_fetch_sts_history_callback() {
+
+	$wp_list_table = new My_List_Table();
+	$wp_list_table->ajax_response();
+
+}
+
+add_action( 'wp_ajax__ajax_fetch_sts_history', '_ajax_fetch_sts_history_callback' );
+
+/**
+ * Action wp_ajax for fetching the first time table structure
+ */
+
+function _ajax_sts_display_callback() {
+
+	check_ajax_referer( 'ajax-custom-list-nonce', '_ajax_custom_list_nonce', true );
+
+	$wp_list_table = new My_List_Table();
+	$wp_list_table->prepare_items();
+
+	ob_start();
+	$wp_list_table->display();
+	$display = ob_get_clean();
+
+	die(
+
+		json_encode(array(
+
+			"display" => $display
+
+		))
+
+	);
+
+}
+
+add_action('wp_ajax__ajax_sts_display', '_ajax_sts_display_callback');
+
+/**
+ * fetch_ts_script function based from Charlie's original function
+ */
+
+function fetch_ts_script() {
+	$screen = get_current_screen();
+
+	/**
+	 * For testing purpose, finding Screen ID
+	 */
+
+	?>
+
+	<script type="text/javascript">console.log("<?php echo $screen->id; ?>")</script>
+
+	<?php
+
+	if ( $screen->id != "toplevel_page_wp_ajax_list_test" )
+		return;
+
+	?>
+
+	<script type="text/javascript">
+
+		(function ($) {
+
+			list = {
+
+				/** added method display
+				 * for getting first sets of data
+				 **/
+
+				display: function() {
+
+					$.ajax({
+
+						url: myajaxurl,
+						dataType: 'json',
+						data: {
+							_ajax_custom_list_nonce: $('#_ajax_custom_list_nonce').val(),
+							action: '_ajax_sts_display'
+						},
+						success: function (response) {
+
+							$("#ts-history-table").html(response.display);
+
+							$("tbody").on("click", ".toggle-row", function(e) {
+								e.preventDefault();
+								$(this).closest("tr").toggleClass("is-expanded")
+							});
+
+							list.init();
+						}
+					});
+
+				},
+
+				init: function () {
+
+					var timer;
+					var delay = 500;
+
+					$('.tablenav-pages a, .manage-column.sortable a, .manage-column.sorted a').on('click', function (e) {
+						e.preventDefault();
+						var query = this.search.substring(1);
+
+						var data = {
+							paged: list.__query( query, 'paged' ) || '1',
+							order: list.__query( query, 'order' ) || 'asc',
+							orderby: list.__query( query, 'orderby' ) || 'title'
+						};
+						list.update(data);
+					});
+
+					$('input[name=paged]').on('keyup', function (e) {
+
+						if (13 == e.which)
+							e.preventDefault();
+
+						var data = {
+							paged: parseInt($('input[name=paged]').val()) || '1',
+							order: $('input[name=order]').val() || 'asc',
+							orderby: $('input[name=orderby]').val() || 'title'
+						};
+
+						window.clearTimeout(timer);
+						timer = window.setTimeout(function () {
+							list.update(data);
+						}, delay);
+					});
+
+					$('#email-sent-list').on('submit', function(e){
+
+						e.preventDefault();
+
+					});
+
+				},
+
+				/** AJAX call
+				 *
+				 * Send the call and replace table parts with updated version!
+				 *
+				 * @param    object    data The data to pass through AJAX
+				 */
+				update: function (data) {
+
+					$.ajax({
+
+						url: myajaxurl,
+						data: $.extend(
+							{
+								_ajax_custom_list_nonce: $('#_ajax_custom_list_nonce').val(),
+								action: '_ajax_fetch_sts_history',
+							},
+							data
+						),
+						success: function (response) {
+
+							var response = $.parseJSON(response);
+
+							if (response.rows.length)
+								$('#the-list').html(response.rows);
+							if (response.column_headers.length)
+								$('thead tr, tfoot tr').html(response.column_headers);
+							if (response.pagination.bottom.length)
+								$('.tablenav.top .tablenav-pages').html($(response.pagination.top).html());
+							if (response.pagination.top.length)
+								$('.tablenav.bottom .tablenav-pages').html($(response.pagination.bottom).html());
+
+							list.init();
+						}
+					});
+				},
+
+				/**
+				 * Filter the URL Query to extract variables
+				 *
+				 * @see http://css-tricks.com/snippets/javascript/get-url-variables/
+				 *
+				 * @param    string    query The URL query part containing the variables
+				 * @param    string    variable Name of the variable we want to get
+				 *
+				 * @return   string|boolean The variable value if available, false else.
+				 */
+				__query: function (query, variable) {
+
+					var vars = query.split("&");
+					for (var i = 0; i < vars.length; i++) {
+						var pair = vars[i].split("=");
+						if (pair[0] == variable)
+							return pair[1];
+					}
+					return false;
+				},
+			}
+
+			list.display();
+
+		})(jQuery);
+
+	</script>
+	<?php
+}
+
+add_action('admin_footer', 'fetch_ts_script');
