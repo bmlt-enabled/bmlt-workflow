1d6a04d39396738a76920fdb442183ef83a1f9e1
Nigel <nigel.bmlt@gmail.com>
2022-03-02 20:14:03 +1030

    update

--- admin/admin_rest_controller.php
+++ admin/admin_rest_controller.php
@@ -67,6 +67,7 @@ class bmaw_submissions_rest extends WP_REST_Controller {
 	public function get_items_permissions_check( $request ) {
         $data = get_userdata( get_current_user_id() );
 error_log("before");
+error_log(print_r($data));
 if ( is_object( $data) ) {
     error_log("after");
 
