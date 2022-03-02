a9f57a028db30cec33d78fadbc6420acb619ad93
Nigel <nigel.bmlt@gmail.com>
2022-03-02 21:57:35 +1030

    update

--- admin/admin_submissions.php
+++ admin/admin_submissions.php
@@ -262,9 +262,9 @@ function _display_bmaw_admin_submissions_page() {
 
 		<form id="email-sent-list" method="get">
 
-			<input type="hidden" name="page" value="<?php echo $_REQUEST['page']; ?>" />
+			<!-- <input type="hidden" name="page" value="<?php echo $_REQUEST['page']; ?>" />
 			<input type="hidden" name="order" value="<?php echo $_REQUEST['order']; ?>" />
-			<input type="hidden" name="orderby" value="<?php echo $_REQUEST['orderby']; ?>" />
+			<input type="hidden" name="orderby" value="<?php echo $_REQUEST['orderby']; ?>" /> -->
 
 			<div id="ts-history-table" style="">
 				<?php
