1f5f2091c1fa1080efed58fb99ba7c68b7fa8df7
Nigel <nigel.bmlt@gmail.com>
2022-03-04 00:41:32 +1030

    update

--- admin/admin_rest_controller.php
+++ admin/admin_rest_controller.php
@@ -179,7 +179,7 @@ class bmaw_submissions_rest extends WP_REST_Controller
 
 	public function post_submissions($request)
 	{
-		error_log($this->vdump($request));
+		error_log($this->vdump($request->get_body_params()));
 
 		meeting_update_form_handler_rest("data");
 
