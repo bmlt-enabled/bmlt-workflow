84c1a9cba18cbdeda1933873e596b70ca5668737
Nigel <nigel.bmlt@gmail.com>
2022-03-03 15:14:33 +1030

    update

--- js/admin_submissions.js
+++ js/admin_submissions.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function ($) {
   $('#approve').click(function(event){
     event.preventDefault(); 
-    $.post('/wp-json/bmaw-submission/v1/submissions/12/approve', function(response){
+    $.post('/flop/wp-json/bmaw-submission/v1/submissions/12/approve', function(response){
        alert(response);
     });
  });
