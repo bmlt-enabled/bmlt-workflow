892955e2514ec73ca68b4e7f1334a5462e1adb35
Nigel <nigel.bmlt@gmail.com>
2022-03-03 11:46:21 +1030

    update rest controller

--- admin/admin_rest_controller.php
+++ admin/admin_rest_controller.php
@@ -32,44 +32,71 @@ class bmaw_submissions_rest extends WP_REST_Controller {
 	public function __construct() {
 
 		$this->namespace = 'bmaw-submission/v1';
-		$this->rest_base = 'submission';
+		$this->rest_base = 'submissions';
 	}
 	/**
 	 * Register the routes for the objects of the controller.
 	 */
 	public function register_routes() {
 
+        // submissions/
 		register_rest_route( $this->namespace, '/' . $this->rest_base, array(
 
 			array(
 				'methods'             => WP_REST_Server::READABLE,
-				'callback'            => array( $this, 'get_items' ),
-				 'permission_callback' => array( $this, 'get_items_permissions_check' ),
+				'callback'            => array( $this, 'get_submissions' ),
+				 'permission_callback' => array( $this, 'get_submissions_permissions_check' ),
 			),
-			// array(
-			// 	'methods'         => WP_REST_Server::EDITABLE,
-			// 	'callback'        => array( $this, 'update_item' ),
-			// 	'permission_callback' => array( $this, 'update_item_permissions_check' ),
-			// 	'args'            => $this->get_endpoint_args_for_item_schema( false ),
-			// ),
+			array(
+				'methods'         => WP_REST_Server::EDITABLE,
+				'callback'        => array( $this, 'post_submissions' ),
+				'permission_callback' => array( $this, 'post_submissions_permissions_check' ),
+				'args'            => $this->get_endpoint_args_for_item_schema( false ),
+			),
 			'schema' => null,
 
 		) );
+        // submissions/<id>
+        register_rest_route( $this->namespace, '/' . $this->rest_base . '/(?P<id>[\d]+)', array(
+            'methods'             => WP_REST_Server::READABLE,
+            'callback'            => array( $this, 'get_submission' ),
+             'permission_callback' => array( $this, 'get_submissions_permissions_check' ),
+        ) );
+        // submissions/<id>/approve
+        register_rest_route( $this->namespace, '/' . $this->rest_base . '/(?P<id>[\d]+)/approve', array(
+            'methods'             => WP_REST_Server::CREATABLE,
+            'callback'            => array( $this, 'approve_submission' ),
+             'permission_callback' => array( $this, 'post_submissions_action_permissions_check' ),
+        ) );
+        // submissions/<id>/reject
+        register_rest_route( $this->namespace, '/' . $this->rest_base . '/(?P<id>[\d]+)/reject', array(
+            'methods'             => WP_REST_Server::CREATABLE,
+            'callback'            => array( $this, 'reject_submission' ),
+             'permission_callback' => array( $this, 'post_submissions_action_permissions_check' ),
+        ) );
 
 	}
 	/**
-	 * Check permissions for the read.
+	 * Check permissions for submission management. These are general purpose checks for all submission editors, granular edit permission will be checked within the callback itself.
 	 *
 	 * @param WP_REST_Request $request get data from request.
 	 *
 	 * @return bool|WP_Error
 	 */
-	public function get_items_permissions_check( $request ) {
+	public function get_submissions_permissions_check( $request ) {
 		if ( ! current_user_can( 'manage_options' ) ) {
-			return new WP_Error( 'rest_forbidden', esc_html__( 'You cannot view the submission resource.' ), array( 'status' => $this->authorization_status_code() ) );
+			return new WP_Error( 'rest_forbidden', esc_html__( 'You cannot view the submissions resource.' ), array( 'status' => $this->authorization_status_code() ) );
 		}
 		return true;
 	}
+
+    public function post_submissions_action_permissions_check( $request ) {
+		if ( ! current_user_can( 'manage_options' ) ) {
+			return new WP_Error( 'rest_forbidden', esc_html__( 'You cannot manage the submissions resource.' ), array( 'status' => $this->authorization_status_code() ) );
+		}
+		return true;
+	}
+
 	/**
 	 * Check permissions for the update
 	 *
@@ -77,10 +104,8 @@ class bmaw_submissions_rest extends WP_REST_Controller {
 	 *
 	 * @return bool|WP_Error
 	 */
-	public function update_item_permissions_check( $request ) {
-		if ( ! current_user_can( 'manage_options' ) ) {
-			return new WP_Error( 'rest_forbidden', esc_html__( 'You cannot update the submission resource.' ), array( 'status' => $this->authorization_status_code() ) );
-		}
+	public function post_submissions_permissions_check( $request ) {
+        // Anyone can post a form submission
 		return true;
 	}
 	/**
@@ -90,7 +115,8 @@ class bmaw_submissions_rest extends WP_REST_Controller {
 	 *
 	 * @return mixed|WP_REST_Response
 	 */
-	public function get_items( $request ) {
+
+	public function get_submissions( $request ) {
 
         global $wpdb;
         global $bmaw_submissions_table_name;
@@ -101,30 +127,46 @@ class bmaw_submissions_rest extends WP_REST_Controller {
 		return rest_ensure_response( $result );
 	}
 
+    public function get_submission( $request ) {
+
+        global $wpdb;
+        global $bmaw_submissions_table_name;
+        $sql = $wpdb->prepare('SELECT * FROM '.$bmaw_submissions_table_name.' where id="%d" limit 1',$request['id']);
+        $result = $wpdb->get_results($sql, ARRAY_A);
+
+		// Return all of our comment response data.
+		return rest_ensure_response( $result );
+	}
+
 	/**
-	 * Update category order
+	 * Form post
 	 *
 	 * @param WP_REST_Request $request get data from request.
 	 *
 	 * @return mixed|WP_Error|WP_REST_Response
 	 */
-	// public function update_item( $request ) {
+
+	public function post_submissions( $request ) {
 
         
-	// 	$data = [];
-	// 	if ( ! isset( $request['order'] ) ) {
-	// 		return new WP_Error( 'invalid_data', __( 'Cannot update category order.' ), array( 'status' => 400 ) );
-	// 	}
-	// 	$res = update_option( 'category_order', $request['order'] );
+		$data = [];
 
-	// 	if ( $res ) {
-	// 		$data['msg'] = __( 'Category order updated', '' );
-	// 	} else {
-	// 		return new WP_Error( 'cant update', __( 'Please provide proper data' ), array( 'status' => 400 ) );
-	// 	}
+        meeting_update_form_handler_rest($data);
 
-	// 	return rest_ensure_response( $data );
-	// }
+		// if ( ! isset( $request['order'] ) ) {
+		// 	return new WP_Error( 'invalid_data', __( 'Cannot update category order.' ), array( 'status' => 400 ) );
+		// }
+		// $res = update_option( 'category_order', $request['order'] );
+
+		// if ( $res ) {
+		// 	$data['msg'] = __( 'Category order updated', '' );
+		// } else {
+		// 	return new WP_Error( 'cant update', __( 'Please provide proper data' ), array( 'status' => 400 ) );
+		// }
+
+		return rest_ensure_response( $data );
+	}
+
 	/**
 	 * Sets up the proper HTTP status code for authorization.
 	 *
