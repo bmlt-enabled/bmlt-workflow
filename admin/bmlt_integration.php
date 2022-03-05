b4ba899e801d33dcb1239f1acc49fb83f9f6d355
Nigel <nigel.bmlt@gmail.com>
2022-03-05 22:23:26 +1030

    update

--- admin/bmlt_integration.php
+++ admin/bmlt_integration.php
@@ -10,11 +10,12 @@ class BMLTIntegration
     protected $authfailure = 0; // authentication is failing - don't keep bashing the front door
     protected const MAXFAILS = 2; // how many failures we'll retry
 
+    // postargs is an array
     public function postConfiguredRootServerRequest($url, $postargs)
     {
         if ($this->authfailure < self::MAXFAILS)
         {    
-            return $this->postRooServerRequest(get_option('bmaw_bmlt_server_address') . $url, $postargs);
+            return $this->postRootServerRequest(get_option('bmaw_bmlt_server_address') . $url, $postargs);
         }
         else
         {
@@ -22,6 +23,17 @@ class BMLTIntegration
         }
     }
 
+    public function postConfiguredRootServerRequestSemantic($url, $postargs)
+    {
+        if ($this->authfailure < self::MAXFAILS)
+        {    
+            return $this->postRootServerRequestSemantic(get_option('bmaw_bmlt_server_address') . $url, $postargs);
+        }
+        else
+        {
+            return new WP_Error( 'bmaw', "Fatal BMLT Authentication Failure - check the credentials in BMAW settings" );
+        }
+    }
     public function getConfiguredRootServerRequest($url)
     {
         if ($this->authfailure < self::MAXFAILS)
@@ -121,6 +133,35 @@ class BMLTIntegration
         return $ret;
     }
 
+    private function postsemantic($url, $cookies = null, $postargs)
+    {
+        error_log("POSTING SEMANTIC URL = " . $url);
+        error_log($this->vdump($this->set_args($cookies, http_build_query($postargs))));
+        error_log("*********");
+        $newargs = '';
+        foreach ($postargs as $key => $value)
+        {
+            switch ($key)
+            {
+                case ('admin_action'):
+                case ('meeting_id'):
+                    $newargs .= $key.'='.$value;
+                    break;
+                default:
+                    $newargs .= "meeting_field[]=".$key.','.$value;
+            }
+            $newargs .= '&';
+        }
+        if ($newargs != '')
+        {   
+            // chop trailing &
+            $newargs = substr($newargs,0,-1);
+            error_log("our post body is ".$newargs);
+            $ret = wp_remote_post($url, $this->set_args($cookies, $newargs));
+            return $ret;
+        }
+    }
+
     private function getRootServerRequest($url)
     {
         error_log("GETROOTSERVERREQUEST COOKIES");
@@ -133,7 +174,7 @@ class BMLTIntegration
         return $this->get($url, $this->cookies);
     }
 
-    private function postRooServerRequest($url, $postargs)
+    private function postRootServerRequest($url, $postargs)
     {
         $ret =  $this->authenticateRootServer();
         if( is_wp_error( $ret ) ) {
@@ -141,6 +182,16 @@ class BMLTIntegration
         }
         return $this->post($url, $this->cookies, $postargs);
     }
+
+    private function postRootServerRequestSemantic($url, $postargs)
+    {
+        $ret =  $this->authenticateRootServer();
+        if( is_wp_error( $ret ) ) {
+            return $ret;
+        }
+        return $this->postsemantic($url, $this->cookies, $postargs);
+    }
+
 }
 
 ?>
