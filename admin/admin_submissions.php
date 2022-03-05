1d19db3897df1a12917a12c63dfb1f199b3221b9
Nigel <nigel.bmlt@gmail.com>
2022-03-05 21:44:05 +1030

    update js

--- admin/admin_submissions.php
+++ admin/admin_submissions.php
@@ -100,9 +100,9 @@ class bmaw_meeting_submissions_page extends WP_List_Table
 
         // Get actions.
         $actions = array(
-            '1'   => '<a class="bmaw_approve" id="bmaw_approve_id_"'.$item['id'].'" target="_blank" href="#">' . esc_html__('Approve') . '</a>',
-            '2'   => '<a class="bmaw_reject" id="bmaw_reject_id_"'.$item['id'].'" target="_blank" href="#">' . esc_html__('Reject') . '</a>',
-            '3'   => '<a class="bmaw_quickedit" id="bmaw_quickedit_id_"'.$item['id'].'" target="_blank" href="#">' . esc_html__('Quick Edit') . '</a>',
+            '1'   => '<a class="bmaw_approve" id="bmaw_approve_id_'.$item['id'].'" target="_blank" href="#">' . esc_html__('Approve') . '</a>',
+            '2'   => '<a class="bmaw_reject" id="bmaw_reject_id_'.$item['id'].'" target="_blank" href="#">' . esc_html__('Reject') . '</a>',
+            '3'   => '<a class="bmaw_quickedit" id="bmaw_quickedit_id_'.$item['id'].'" target="_blank" href="#">' . esc_html__('Quick Edit') . '</a>',
         );
 
         $row_actions = array();
