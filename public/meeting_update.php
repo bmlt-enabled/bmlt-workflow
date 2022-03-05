550689eb002aabe7082789be1420408a97c8f69f
Nigel <nigel.bmlt@gmail.com>
2022-03-05 17:57:32 +1030

    update

--- public/meeting_update.php
+++ public/meeting_update.php
@@ -174,18 +174,18 @@ echo 'var bmaw_bmlt_server_address = "' . get_option('bmaw_bmlt_server_address')
                         		$response = $bmlt_integration->postConfiguredRootServerRequest('local_server/server_admin/json.php', array('admin_action'=>'get_format_info'));
 
                                 $formatarr = json_decode($response['body'],true)['row'];
-                               error_log(vdump($formatarr));
-                                // foreach ($formatarr['row'] as $key => $value)
-                                // {
-                                //     error_log("key ".$key);
-                                //     error_log(vdump($value));
-                                //     $row = '<tr>';
-                                //     $row .= '<td><input type="checkbox" id="format-table-' + $key; '" value="'+ $value['formatcode'] + '"></input></td>';
-                                //     $row .= "<td>(" + $value['formatcode'] +")</td>";
-                                //     $row .= "<td>" + $value['name'] + "</td><td>" + $value['description'] +"</td>";
-                                //     $row .= '</tr>';
-                                //     echo $row;
-                                // }
+                            //    error_log(vdump($formatarr));
+                                foreach ($formatarr as $key => $value)
+                                {
+                                    error_log("key ".$key);
+                                    error_log(vdump($value));
+                                    $row = '<tr>';
+                                    $row .= '<td><input type="checkbox" id="format-table-' + $key; '" value="'+ $value['formatcode'] + '"></input></td>';
+                                    $row .= "<td>(" + $value['formatcode'] +")</td>";
+                                    $row .= "<td>" + $value['name'] + "</td><td>" + $value['description'] +"</td>";
+                                    $row .= '</tr>';
+                                    echo $row;
+                                }
                         ?>
                     </tbody>
                 </table>
