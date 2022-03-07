5e6ad78cc8ea1353c026206dcab2188a4c222c37
Nigel <nigel.bmlt@gmail.com>
2022-03-07 16:22:32 +1030

    css

--- js/admin_submissions.js
+++ js/admin_submissions.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function ($) {
 
-  $(".bmaw_approve").click(function (event) {
+  $(".bmaw_submission_approve").click(function (event) {
     event.preventDefault();
     var id = this.id.replace("bmaw_approve_id_", "");
     $.post("/flop/wp-json/bmaw-submission/v1/submissions/" + id + "/approve", {
