06f5d353acd208ec2221070f495827d41822622f
Nigel <nigel.bmlt@gmail.com>
2022-03-08 21:39:59 +1030

    update

--- admin/admin_service_areas.php
+++ admin/admin_service_areas.php
@@ -12,7 +12,12 @@ $change['admin_action']='get_service_body_info';
 $bmlt_integration = new BMLTIntegration;
 
 $response = $bmlt_integration->postConfiguredRootServerRequestSemantic('local_server/server_admin/json.php', $change);
+if( is_wp_error( $response ) ) {
+    wp_die("BMLT Configuration Error - Unable to retrieve meeting formats");
+}
 
-print_r($response);
+$arr = json_decode($response['body'],true);
+
+var_dump($arr);
 
 ?>
