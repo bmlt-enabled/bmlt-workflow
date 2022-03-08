c9f62df7df73c18d3599e7fea922297a219c9a20
Nigel <nigel.bmlt@gmail.com>
2022-03-08 21:26:39 +1030

    update

---
+++ admin/admin_service_areas.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('ABSPATH')) exit; // die if being called directly
+
+if (!class_exists('BMLTIntegration')) {
+	require_once(BMAW_PLUGIN_DIR . 'admin/bmlt_integration.php');
+}
+
+wp_nonce_field('wp_rest', '_wprestnonce');
+
+$change['admin_action']='get_service_body_info';
+
+$response = $this->bmlt_integration->postConfiguredRootServerRequestSemantic('local_server/server_admin/json.php', $change);
+
+print_r($response);
+
+?>
