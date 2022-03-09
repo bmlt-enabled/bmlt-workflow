c143d7a4f2d74ae7690c3d25d86278a08984dcb8
Nigel <nigel.bmlt@gmail.com>
2022-03-09 20:25:39 +1100

    update

--- js/admin_service_areas.js
+++ js/admin_service_areas.js
@@ -2,7 +2,10 @@ jQuery(document).ready(function ($) {
     $('#bmaw-userlist-1').select2({
         ajax: {
           url: bmaw_admin_bmaw_users_rest_url,
-          dataType: 'json'
+          dataType: 'json',
+          beforeSend: function (xhr) {
+            xhr.setRequestHeader("X-WP-Nonce", $("#_wprestnonce").val());
+          },    
           // Additional AJAX parameters go here; see the end of this chapter for the full code of this example
         }
       });
