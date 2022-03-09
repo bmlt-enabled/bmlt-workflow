94b3c34c0a3b8e3c94f7450e0bc0c620ddc41660
Nigel <nigel.bmlt@gmail.com>
2022-03-09 22:52:27 +1100

    update

--- js/admin_service_areas.js
+++ js/admin_service_areas.js
@@ -1,13 +1,17 @@
 
 jQuery(document).ready(function ($) {
+  $("bmaw-userlist").each(function() {
+    console.log("found "+ $(this).attr('id'));
+  });
+
     $('.bmaw-userlist').select2({
-        ajax: {
-          url: function () { return bmaw_admin_bmaw_service_areas_rest_url + "/" + $(this).attr('id').substring($(this).attr('id').indexOf("_id_") + 4, $(this).attr('id').length) },
-          dataType: 'json',
-          beforeSend: function (xhr) {
-            xhr.setRequestHeader("X-WP-Nonce", $("#_wprestnonce").val());
-          },    
-          // Additional AJAX parameters go here; see the end of this chapter for the full code of this example
-        }
+        // ajax: {
+        //   url: function () { return bmaw_admin_bmaw_service_areas_rest_url + "/" + $(this).attr('id').substring($(this).attr('id').indexOf("_id_") + 4, $(this).attr('id').length) },
+        //   dataType: 'json',
+        //   beforeSend: function (xhr) {
+        //     xhr.setRequestHeader("X-WP-Nonce", $("#_wprestnonce").val());
+        //   },    
+        //   // Additional AJAX parameters go here; see the end of this chapter for the full code of this example
+        // }
       });
     });
