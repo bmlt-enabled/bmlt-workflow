902c9f260dd0eb73388bc4165cd249539b103d2e
Nigel <nigel.bmlt@gmail.com>
2022-03-09 14:19:56 +1030

    update

--- admin/admin_submissions.php
+++ admin/admin_submissions.php
@@ -7,17 +7,25 @@ $submissionsListTable = new bmaw_meeting_submissions_page();
 wp_nonce_field('wp_rest', '_wprestnonce');
 
 ?>
-
+// Approve dialog
 <div id="bmaw_submission_approve_dialog" class="hidden" style="max-width:800px">
   <label class='dialog_label' for="bmaw_submission_approve_dialog_textarea">Approval note:</label>
   <textarea class='dialog_textarea' id="bmaw_submission_approve_dialog_textarea" rows=5 cols=60 placeholder='Add a note to this approval for the submitter'></textarea>
-<p>You can use the quickedit function to make changes before approval.</p><br>
-<p>Are you sure you would like to approve the submission directly?</p>
+<p>You can use the quickedit function to make any extra changes before approval.</p>
+<p>Are you sure you would like to approve the submission?</p>
 </div>
 
+// Delete dialog
 <div id="bmaw_submission_delete_dialog" class="hidden" style="max-width:800px">
   <p>This change cannot be undone. Use this to remove an entirely unwanted submission from the list.</p>
   <p>Are you sure you would like to delete the submission completely?</p>
+</div>
+
+// Reject dialog
+<div id="bmaw_submission_reject_dialog" class="hidden" style="max-width:800px">
+  <label class='dialog_label' for="bmaw_submission_reject_dialog_textarea">Rejection note:</label>
+  <textarea class='dialog_textarea' id="bmaw_submission_reject_dialog_textarea" rows=5 cols=60 placeholder='Add a note to this rejection for the submitter'></textarea>
+<p>Are you sure you would like to reject this submission?</p>
 </div>
 
 <div class="wrap">
@@ -80,7 +88,7 @@ class bmaw_meeting_submissions_page extends WP_List_Table
             'id'          => 'ID',
             'submitter_name'       => 'Submitter Name',
             'submitter_email' => 'Submitter Email',
-            'submission_type'        => 'Change Type',
+            // 'submission_type'        => 'Change Type',
             'change_summary' => 'Change Summary',
             'submission_time'    => 'Submission Time',
             'change_time' => 'Change Time',
@@ -147,7 +155,7 @@ class bmaw_meeting_submissions_page extends WP_List_Table
                 // case 'id':
             case 'submitter_name':
             case 'submitter_email':
-            case 'submission_type':
+            // case 'submission_type':
             case 'submission_date_time':
             case 'change_summary':
             case 'change_time':
@@ -192,7 +200,7 @@ class bmaw_meeting_submissions_page extends WP_List_Table
             $change = unserialize($value['changes_requested']);
             // error_log("deserialised");
             // error_log(vdump($change));
-            $summary = '';
+            $summary = 'Change Type: <b>' . $value['submission_type'] . '</b><br>';
             foreach ($change as $key2 => $value2)
             {
                 // skip meeting_id as it is always required
