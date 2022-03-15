26873239d0d5d2501a07fc3aa1fdb4f94ff3530f
Nigel <nigel.bmlt@gmail.com>
2022-03-15 12:55:12 +1100

    debug

--- admin/admin_rest_handlers.php
+++ admin/admin_rest_handlers.php
@@ -106,7 +106,7 @@ class bmaw_submissions_rest_handlers
         }
         // add / remove user capabilities
         $users = get_users();
-        $sql = $wpdb->query('SELECT DISTINCT wp_uid from'. $bmaw_service_areas_access_table_name);
+        $sql = $wpdb->query('SELECT DISTINCT wp_uid from '. $bmaw_service_areas_access_table_name);
 		$result = $wpdb->get_results($sql, ARRAY_N);
         error_log(vdump($result));
         foreach ($users as $user)
