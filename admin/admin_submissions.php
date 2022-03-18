ef63ef5b00e8cc8e9b51e42741ae79fd5653f820
Nigel <nigel.bmlt@gmail.com>
2022-03-18 16:40:33 +1100

    commit to the datatables design

--- admin/admin_submissions.php
+++ admin/admin_submissions.php
@@ -2,8 +2,6 @@
 
 if (!defined('ABSPATH')) exit; // die if being called directly
 
-// $submissionsListTable = new bmaw_meeting_submissions_page();
-
 wp_nonce_field('wp_rest', '_wprestnonce');
 
 ?>
@@ -32,31 +30,13 @@ wp_nonce_field('wp_rest', '_wprestnonce');
     <div id="icon-users" class="icon32"></div>
     <h2>Meeting Submissions</h2>
     <hr class="wp-header-end">
-    <!-- $actions['bmaw_span_approve'] = '<a class="bmaw_submission_approve" id="bmaw_submission_approve_id_' . $item['id'] . '" target="_blank" href="#!">' . esc_html__('Approve') . '</a>'; -->
-
-    <?php
-    // $submissionsListTable->views();
-    // $submissionsListTable->prepare_items();
-    // $submissionsListTable->display();
-    // 'id'          => 'ID',
-    // 'submitter_name'       => 'Submitter Name',
-    // 'submitter_email' => 'Submitter Email',
-    // // 'submission_type'        => 'Change Type',
-    // 'change_summary' => 'Change Summary',
-    // 'submission_time'    => 'Submission Time',
-    // 'change_time' => 'Change Time',
-    // 'changed_by' => 'Changed By',
-    // 'change_made' => 'Change Made'
-
-    ?>
     <div class="dt-container">
-        <table id="dt-submission" class="display" style="width:80%">
+        <table id="dt-submission" class="display" style="width:90%">
             <thead>
                 <tr>
                     <th>ID</th>
                     <th>Submitter Name</th>
                     <th>Submitter Email</th>
-                    <!-- <th>Change Type</th> -->
                     <th>Change Summary</th>
                     <th>Submission Time</th>
                     <th>Change Time</th>
@@ -69,7 +49,6 @@ wp_nonce_field('wp_rest', '_wprestnonce');
                     <th>ID</th>
                     <th>Submitter Name</th>
                     <th>Submitter Email</th>
-                    <!-- <th>Change Type</th> -->
                     <th>Change Summary</th>
                     <th>Submission Time</th>
                     <th>Change Time</th>
@@ -80,206 +59,3 @@ wp_nonce_field('wp_rest', '_wprestnonce');
         </table>
     </div>
 </div>
-<?php
-
-// if (!class_exists('WP_List_Table')) {
-//     require_once(ABSPATH . 'wp-admin/includes/class-wp-list-table.php');
-// }
-
-class bmaw_meeting_submissions_page extends WP_List_Table
-{
-
-    public function prepare_items()
-    {
-        $columns = $this->get_columns();
-        $hidden = $this->get_hidden_columns();
-        $sortable = $this->get_sortable_columns();
-
-        $data = $this->table_data();
-        error_log("TABLE OUTPUT");
-        error_log(vdump($data));
-
-        usort($data, array(&$this, 'sort_data'));
-
-        $perPage = 10;
-        $currentPage = $this->get_pagenum();
-        $totalItems = count($data);
-
-        $this->set_pagination_args(array(
-            'total_items' => $totalItems,
-            'per_page'    => $perPage
-        ));
-
-        $data = array_slice($data, (($currentPage - 1) * $perPage), $perPage);
-
-        $this->_column_headers = array($columns, $hidden, $sortable);
-        $this->items = $data;
-    }
-
-    // id mediumint(9) NOT NULL AUTO_INCREMENT,
-    // submission_time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-    // change_time datetime DEFAULT '0000-00-00 00:00:00',
-    // changed_by varchar(10),
-    // change_made varchar(10),
-    // submitter_name tinytext NOT NULL,
-    // submission_type tinytext NOT NULL,
-    // submitter_email varchar(320) NOT NULL,
-
-    public function get_columns()
-    {
-        $columns = array(
-            'id'          => 'ID',
-            'submitter_name'       => 'Submitter Name',
-            'submitter_email' => 'Submitter Email',
-            // 'submission_type'        => 'Change Type',
-            'change_summary' => 'Change Summary',
-            'submission_time'    => 'Submission Time',
-            'change_time' => 'Change Time',
-            'changed_by' => 'Changed By',
-            'change_made' => 'Change Made'
-        );
-
-        return $columns;
-    }
-
-    public function get_hidden_columns()
-    {
-        return array();
-    }
-
-    public function get_sortable_columns()
-    {
-        return array(
-            'id' => array('id', 'num'),
-            'submission_time' => array('submission_time', 'asc'),
-            'submitter_name' => array('submitter_name', 'asc'),
-            'submitter_email' => array('submitter_email', 'asc'),
-        );
-    }
-
-    public function column_id($item)
-    {
-        $output    = '';
-        // error_log(vdump($item));
-        $row_actions = array();
-
-        // Title.
-        $output .= '<strong><a href="#" class="row-title">' . esc_html($item['id']) . '</a></strong>';
-
-        $actions = array();
-        // if($item['change_made'] != 'Approved')
-        // {
-        $actions['bmaw_span_approve'] = '<a class="bmaw_submission_approve" id="bmaw_submission_approve_id_' . $item['id'] . '" target="_blank" href="#!">' . esc_html__('Approve') . '</a>';
-        // }
-        if ($item['change_made'] != 'Rejected') {
-            $actions['bmaw_span_reject'] = '<a class="bmaw_submission_reject" id="bmaw_submission_reject_id_' . $item['id'] . '" target="_blank" href="#!">' . esc_html__('Reject') . '</a>';
-        }
-        // if($item['change_made'] = '')
-        // {
-        $actions['bmaw_span_quickedit'] = '<a class="bmaw_submission_quickedit" id="bmaw_submission_quickedit_id_' . $item['id'] . '" target="_blank" href="#!">' . esc_html__('Quick Edit') . '</a>';
-        // }
-        $actions['bmaw_span_delete'] = '<a class="bmaw_submission_delete" id="bmaw_submission_delete_id_' . $item['id'] . '" target="_blank" href="#!">' . esc_html__('Delete') . '</a>';
-
-        $row_actions = array();
-
-        foreach ($actions as $action => $link) {
-            $row_actions[] = '<span class="' . esc_attr($action) . '">' . $link . '</span>';
-        }
-        // error_log(vdump($row_actions));
-        $output .= '<div class="row-actions">' . implode(' | ', $row_actions) . '</div>';
-
-        return $output;
-    }
-
-    public function column_default($item, $column_name)
-    {
-        switch ($column_name) {
-                // case 'id':
-            case 'submitter_name':
-            case 'submitter_email':
-                // case 'submission_type':
-            case 'submission_date_time':
-            case 'change_summary':
-            case 'change_time':
-            case 'changed_by':
-            case 'change_made':
-            case 'submission_time':
-                return $item[$column_name];
-
-            default:
-                return;
-        }
-    }
-
-    private function vdump($object)
-    {
-        ob_start();
-        var_dump($object);
-        $contents = ob_get_contents();
-        ob_end_clean();
-        return $contents;
-    }
-
-    protected function get_views()
-    {
-        $status_links = array(
-            "all"       => __("<a href='#'>All</a>", 'bmaw-submissions'),
-            "not approved" => __("<a href='#'>Not Approved</a>", 'bmaw-submissions')
-            // add class='current'
-            // add total post numbers
-
-        );
-        return $status_links;
-    }
-
-    private function table_data()
-    {
-        $request  = new WP_REST_Request('GET', '/bmaw-submission/v1/submissions');
-        $response = rest_do_request($request);
-        $result     = rest_get_server()->response_to_data($response, true);
-        // format the changes requested
-        foreach ($result as $key => $value) {
-            $change = unserialize($value['changes_requested']);
-            // error_log("deserialised");
-            // error_log(vdump($change));
-            $summary = '<b>Change Type: ' . $value['submission_type'] . '</b><br><br>';
-            foreach ($change as $key2 => $value2) {
-                // skip meeting_id as it is always required
-                if ($key2 != 'meeting_id') {
-                    $summary .= $key2 . ' <span class="dashicons dashicons-arrow-right-alt"></span> <b>' . $value2 . '</b><br>';
-                }
-            }
-            // chop trailing <br>
-            $result[$key]['change_summary'] = substr($summary, 0, -4);;
-        }
-
-        return $result;
-    }
-
-    private function sort_data($a, $b)
-    {
-        // Set defaults
-        $orderby = 'id';
-        $order = 'asc';
-
-        // If orderby is set, use this as the sort column
-        if (!empty($_GET['orderby'])) {
-            $orderby = $_GET['orderby'];
-        }
-
-        // If order is set use this as the order
-        if (!empty($_GET['order'])) {
-            $order = $_GET['order'];
-        }
-
-
-        $result = strnatcmp($a[$orderby], $b[$orderby]);
-
-        if ($order === 'asc') {
-            return $result;
-        }
-
-        return -$result;
-    }
-}
-?>
