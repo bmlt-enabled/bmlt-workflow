89d73738a4477e8e7737b6d24ab273e88223340b
Nigel <nigel.bmlt@gmail.com>
2022-03-18 21:58:28 +1100

    bugfix

--- js/admin_submissions.js
+++ js/admin_submissions.js
@@ -191,8 +191,12 @@ jQuery(document).ready(function ($) {
   $("#dt-submission")
     .DataTable()
     .on("select deselect", function () {
-      var change_made = $("#dt-submission").DataTable().row({ selected: true }).data()["change_made"];
-      var not_actioned = ((change_made !== 'Approved')||(change_made !== 'Rejected'))
+      var not_actioned = false;
+      if ($("#dt-submission").DataTable().row({ selected: true }).count())
+      {
+        var change_made = $("#dt-submission").DataTable().row({ selected: true }).data()["change_made"];
+        var not_actioned = ((change_made !== 'Approved')||(change_made !== 'Rejected'))
+      }
       $("#dt-submission")
         .DataTable().button('approve:name')
         .enable(not_actioned);
