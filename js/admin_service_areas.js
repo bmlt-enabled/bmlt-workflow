45b9f7948133494f54daddb71e0b90ce18c0f5a7
Nigel <nigel.bmlt@gmail.com>
2022-03-19 15:29:04 +1100

    add spinner

--- js/admin_service_areas.js
+++ js/admin_service_areas.js
@@ -58,6 +58,11 @@ jQuery(document).ready(function ($) {
     });
   });
 
+  // enable spinner
+  $(document).ajaxSend(function() {
+    $("#overlay").fadeIn(300);　
+  });
+
   // get the permissions, and the userlist from wordpress, and create our select lists
   $.ajax({
     url: wp_rest_base + bmaw_admin_bmaw_service_areas_rest_route,
@@ -74,12 +79,15 @@ jQuery(document).ready(function ($) {
         xhr.setRequestHeader("X-WP-Nonce", $("#_wprestnonce").val());
       },
     }).done(function (response) {
-      console.log("service body list");
-      console.log(this.sblist);
-      console.log("userlist");
+
+      // turn off spinner
+      setTimeout(function(){ $("#overlay").fadeOut(300); },500);
+      // console.log("service body list");
+      // console.log(this.sblist);
+      // console.log("userlist");
       // var select_userlist = { "results": {} };
 
-      console.log(response);
+      // console.log(response);
       var sblist = this.sblist;
       var userlist = response;
       Object.keys(sblist).forEach((item) => {
@@ -105,34 +113,3 @@ jQuery(document).ready(function ($) {
     });
   });
 });
-
-// $request = new WP_REST_Request('GET', '/wp/v2/users');
-// $result = rest_do_request($request);
-
-// $data = $result->get_data();
-// $select = array('results' => array());
-// foreach ($data as $user) {
-// 	$data = array('id' => $user['id'], 'text' => $user['name']);
-// 	// if we have a match from the administration list, mark it as selected
-// 	if (in_array($user['id'], $arr)) {
-// 		$data['selected'] = true;
-// 	}
-// 	$select['results'][] = $data;
-// }
-
-// response["results"].forEach((element) => {
-//   var opt = new Option(element.text, element.id, false, element.selected);
-//   $('#'+this.custom).append(opt).trigger("change");
-
-// $(".bmaw-userlist").each(function () {
-//   console.log("found " + $(this).attr("id"));
-// });
-
-// <?php
-// foreach ($sblist as $item) {
-//     echo '<tr class="bmaw-userlist-row">';
-//     echo '<td>'.$item['name'].'</td>';
-//     echo '<td><select class="bmaw-userlist" id="bmaw_userlist_id_'.$item['id'].'" style="width: auto"></select></td></tr>';
-// }
-// echo '</tr>';
-// ?>
