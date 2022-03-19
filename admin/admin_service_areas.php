3fdb49e2c60561f58084d4908dd60c1b76b44868
Nigel <nigel.bmlt@gmail.com>
2022-03-19 16:32:35 +1100

    update

--- admin/admin_service_areas.php
+++ admin/admin_service_areas.php
@@ -13,8 +13,8 @@ wp_nonce_field('wp_rest', '_wprestnonce');
 <div class="wrap">
     <div id="icon-users" class="icon32"></div>
     <h2>Service Area Configuration</h2>
-    <br><span class="spinner" id="bmaw-form-spinner"></span><br>
-                <table class="bmaw-userlist-table" id="bmaw-userlist-table">
+    <span class="spinner" id="bmaw-form-spinner"></span>
+                <table class="bmaw-userlist-table" id="bmaw-userlist-table" style="display: none;">
                 <thead>
                     <tr>
                         <th class="bmaw-userlist-header">Service Area</th>
--- js/admin_service_areas.js
+++ js/admin_service_areas.js
@@ -15,21 +15,19 @@ jQuery(document).ready(function ($) {
     $(selectid).trigger("change");
   }
 
-  function turn_off_spinner(element)
-  {
-    $(element).removeClass("is-active");　
+  function turn_off_spinner(element) {
+    $(element).removeClass("is-active");
   }
 
-  function turn_on_spinner(element)
-  {
-    $(element).addClass("is-active");　
+  function turn_on_spinner(element) {
+    $(element).addClass("is-active");
   }
 
   function create_service_area_permission_post() {
     ret = {};
     $(".bmaw-userlist").each(function () {
       // console.log("got real id " + $(this).data("id"));
-      id =  $(this).data("id");
+      id = $(this).data("id");
       // console.log("got name " + $(this).data("name"));
       sbname = $(this).data("name");
       // console.log("select vals = "+ $(this).val());
@@ -37,22 +35,22 @@ jQuery(document).ready(function ($) {
       // console.log("got show_on_form = "+ $(this).data("show_on_form"));
       show_on_form = $(this).data("show_on_form");
 
-      ret[id] = { "name":sbname, "show_on_form": show_on_form, "membership":membership};
+      ret[id] = { name: sbname, show_on_form: show_on_form, membership: membership };
     });
     return ret;
   }
 
   $("#bmaw_submit").on("click", function () {
-    $('#bmaw-userlist-table tbody tr').each(function() {
+    $("#bmaw-userlist-table tbody tr").each(function () {
       tr = $(this);
-      checked = $(tr).find('input:checkbox').prop('checked');
-      select = $(tr).find('select');
-      select.data('show_on_form', checked);
+      checked = $(tr).find("input:checkbox").prop("checked");
+      select = $(tr).find("select");
+      select.data("show_on_form", checked);
     });
     post = create_service_area_permission_post();
     $.ajax({
       url: wp_rest_base + bmaw_admin_bmaw_service_areas_rest_route,
-      method: 'POST',
+      method: "POST",
       data: JSON.stringify(post),
       contentType: "application/json; charset=utf-8",
       dataType: "json",
@@ -66,7 +64,6 @@ jQuery(document).ready(function ($) {
       turn_off_spinner("#bmaw-submit-spinner");
     });
   });
-
 
   // get the permissions, and the userlist from wordpress, and create our select lists
   $.ajax({
@@ -85,36 +82,33 @@ jQuery(document).ready(function ($) {
         xhr.setRequestHeader("X-WP-Nonce", $("#_wprestnonce").val());
       },
     }).done(function (response) {
-
-      // turn off spinner
-      turn_off_spinner("#bmaw-form-spinner");
-      // console.log("service body list");
-      // console.log(this.sblist);
-      // console.log("userlist");
-      // var select_userlist = { "results": {} };
-
-      // console.log(response);
       var sblist = this.sblist;
       var userlist = response;
       Object.keys(sblist).forEach((item) => {
         var id = "bmaw_userlist_id_" + item;
-        var checked = sblist[item]["show_on_form"]?("checked"):("");
+        var checked = sblist[item]["show_on_form"] ? "checked" : "";
         var appendstr = "<tr>";
 
         appendstr += "<td>" + sblist[item]["name"] + "</td>";
         appendstr += '<td><select class="bmaw-userlist" id="' + id + '" style="width: auto"></select></td>';
-        appendstr += '<td class="bmaw-center-checkbox"><input type="checkbox" '+checked+'></td>';
+        appendstr += '<td class="bmaw-center-checkbox"><input type="checkbox" ' + checked + "></td>";
         appendstr += "</tr>";
         $("#bmaw-userlist-table tbody").append(appendstr);
         // store metadata away for later
-        $("#"+id).data('id', item);
-        $("#"+id).data('name', sblist[item]["name"]);
-        
+        $("#" + id).data("id", item);
+        $("#" + id).data("name", sblist[item]["name"]);
+
         $(".bmaw-userlist").select2({
           multiple: true,
           width: "100%",
         });
         attach_select_options_for_sbid(sblist, userlist, item, "#" + id);
+
+        // turn off spinner
+        turn_off_spinner("#bmaw-form-spinner");
+        // turn on table
+        $("#bmaw-userlist-table").show();
+        
       });
     });
   });
