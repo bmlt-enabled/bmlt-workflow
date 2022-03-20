97115d5bbb06f7325f6b6da98bf814f1fbdf344d
Nigel <nigel.bmlt@gmail.com>
2022-03-20 14:55:51 +1100

    update

--- js/admin_submissions.js
+++ js/admin_submissions.js
@@ -23,24 +23,24 @@ jQuery(document).ready(function ($) {
   });
   formatdata['results'] = result;
 
-  // formatdata = {
-  //   "results": [
-  //     {
-  //       "id": 1,
-  //       "text": "Option 1"
-  //     },
-  //     {
-  //       "id": 2,
-  //       "text": "Option 2",
-  //       "selected": true
-  //     },
-  //     {
-  //       "id": 3,
-  //       "text": "Option 3",
-  //       "disabled": true
-  //     }
-  //   ]
-  // };
+  formatdata = {
+    "results": [
+      {
+        "id": 1,
+        "text": "Option 1"
+      },
+      {
+        "id": 2,
+        "text": "Option 2",
+        "selected": true
+      },
+      {
+        "id": 3,
+        "text": "Option 3",
+        "disabled": true
+      }
+    ]
+  };
 
   $("#bmaw-formatlist").select2({
     multiple: true,
