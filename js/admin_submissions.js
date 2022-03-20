ff6d8cb7e31c4ea2aec6fb536ba6aea6ae9efa47
Nigel <nigel.bmlt@gmail.com>
2022-03-20 17:56:40 +1100

    changed css

--- js/admin_submissions.js
+++ js/admin_submissions.js
@@ -64,6 +64,9 @@ jQuery(document).ready(function ($) {
         action: function (e, dt, button, config) {
           clear_notices();
           var id = dt.cell(".selected", 0).data();
+          // clear quickedit
+          //
+          // fill quickedit
           $('#meeting_name').val(bmaw_changedata[id].changes_requested['meeting_name']);
           $("#bmaw_submission_quickedit_dialog").data("id", id).dialog("open");
         },
