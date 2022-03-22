256aad37b5ce3318482aa8d9cece65f91b1567f0
Nigel <nigel.bmlt@gmail.com>
2022-03-22 16:46:44 +1100

    update

--- admin/admin_service_areas.php
+++ admin/admin_service_areas.php
@@ -13,6 +13,14 @@ wp_nonce_field('wp_rest', '_wprestnonce');
 <div class="wrap">
     <div id="icon-users" class="icon32"></div>
     <h2>Service Area Configuration</h2>
+    <div class="bmaw_info_text">
+    <br><br>Service areas are retrieved from BMLT using the BMLT details configured on the option page. 
+    <br><br>You can configure which service areas are visible to the end-users using the <code>Display on end-user Form</code> checkbox.
+    <br><br>You can select users from your Wordpress userlist and grant them access in the <code>Wordpress Users with Access</code> section. 
+    These users will only be given access to the submission admin page, and only submissions from their service areas will be visible to approve.
+    <br><br>
+    </div>
+
     <span class="spinner" id="bmaw-form-spinner"></span>
                 <table class="bmaw-userlist-table" id="bmaw-userlist-table" style="display: none;">
                 <thead>
