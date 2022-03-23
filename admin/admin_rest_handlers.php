6198333b9b44047e5150275d264e5580035d8050
Nigel <nigel.bmlt@gmail.com>
2022-03-23 11:21:26 +1100

    access control on submission view

--- admin/admin_rest_handlers.php
+++ admin/admin_rest_handlers.php
@@ -27,8 +27,15 @@ class bmaw_submissions_rest_handlers
 
         global $wpdb;
         global $bmaw_submissions_table_name;
+        global $bmaw_service_areas_access_table_name;
 
-        $result = $wpdb->get_results('SELECT * FROM ' . $bmaw_submissions_table_name, ARRAY_A);
+        // only show submissions we have access to
+        // select * from wp_bmaw_submissions s inner join wp_bmaw_service_areas_access a on s.service_body_bigint = a.service_body_bigint where a.wp_uid = 1
+        $this_user = wp_get_current_user();
+        $current_uid = $this_user->get('ID');
+        $sql = $wpdb->prepare('SELECT * FROM ' . $bmaw_submissions_table_name . ' s inner join '. $bmaw_service_areas_access_table_name . ' a on s.service_body_bigint = a.service_body_bigint where a.wp_uid =%d',$current_uid, ARRAY_A);
+        $result = $wpdb->get_results($sql);
+        // $result = $wpdb->get_results('SELECT * FROM ' . $bmaw_submissions_table_name . ' s inner join wp_bmaw_service_areas_access a on s.service_body_bigint = a.service_body_bigint where a.wp_uid =%d', ARRAY_A);
         error_log(vdump($result));
         foreach ($result as $key => $value) {
             $result[$key]['changes_requested'] = json_decode($result[$key]['changes_requested'], true, 2);
