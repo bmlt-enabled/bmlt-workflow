1d6014d2a6159452b15d6159498374d30c3c1480
Nigel <nigel.bmlt@gmail.com>
2022-03-26 20:46:13 +1100

    fix form submit format

--- tests/admin/meeting_update_form_handlerTest.php
+++ tests/admin/meeting_update_form_handlerTest.php
@@ -44,9 +44,9 @@ final class meeting_update_form_handlerTest extends TestCase
             "submit" => "Submit Form",
             "additional_info" => "I'd like to close the meeting please"
         );
-
         global $wpdb;
         $wpdb =  Mockery::mock( 'wpdb' );
+        /** @var Mockery::mock $wpdb test */
         $wpdb->shouldReceive( 'insert' )->andReturn( array('0'=>'1') )->set('insert_id',10);
         $json = '[{"id_bigint":"3277","worldid_mixed":"OLM297","service_body_bigint":"6","weekday_tinyint":"3","venue_type":"2","start_time":"19:00:00","duration_time":"01:00:00","time_zone":"","formats":"JT,LC,VM","longitude":"151.2437","latitude":"-33.9495","meeting_name":"Online Meeting - Maroubra Nightly","location_text":"Online","location_info":"","location_street":"","location_neighborhood":"","location_municipality":"Maroubra","location_sub_province":"","location_province":"NSW","location_postal_code_1":"2035","comments":"","contact_phone_2":"","contact_email_2":"","contact_name_2":"","contact_phone_1":"","contact_email_1":"","contact_name_1":"","virtual_meeting_additional_info":"By phone 02 8015 6011Meeting ID: 83037287669 Passcode: 096387","root_server_uri":"http://54.153.167.239/main_server","format_shared_id_list":"14,40,54"}]';
         Functions\when('curl_exec')->justReturn($json);
@@ -73,6 +73,7 @@ final class meeting_update_form_handlerTest extends TestCase
 
         global $wpdb;
         $wpdb = Mockery::mock( 'wpdb' );
+        /** @var Mockery::mock $wpdb test */
         $wpdb->shouldReceive( 'insert' )->andReturn( array('0'=>'1') )->set('insert_id',10);
         $response = meeting_update_form_handler_rest($form_post);
         var_dump($response);
@@ -99,6 +100,34 @@ final class meeting_update_form_handlerTest extends TestCase
         // echo date('Y-m-d', time());
         global $wpdb;
         $wpdb = Mockery::mock( 'wpdb' );
+        /** @var Mockery::mock $wpdb test */
+        $wpdb->shouldReceive( 'insert' )->andReturn( array('0'=>'1') )->set('insert_id',10);
+        $response = meeting_update_form_handler_rest($form_post);
+         var_dump($response);
+        $this->assertInstanceOf(WP_REST_Response::class, $response);
+        $this->assertEquals(200, $response->get_status());
+    }
+
+    public function testChangeMeetingCanChangeFormat():void {
+
+        $form_post = array(
+            "action" => "meeting_update_form_response",
+            "update_reason" => "reason_change",
+            "meeting_name" => "testing name change",
+            "meeting_id" => "200",
+            "first_name" => "joe",
+            "last_name" => "joe",
+            "email_address" => "joe@joe.com",
+            "submit" => "Submit Form"
+        );
+
+        $json = '[{"id_bigint":"3277","worldid_mixed":"OLM297","service_body_bigint":"6","weekday_tinyint":"3","venue_type":"2","start_time":"19:00:00","duration_time":"01:00:00","time_zone":"","formats":"JT,LC,VM","longitude":"151.2437","latitude":"-33.9495","meeting_name":"Online Meeting - Maroubra Nightly","location_text":"Online","location_info":"","location_street":"","location_neighborhood":"","location_municipality":"Maroubra","location_sub_province":"","location_province":"NSW","location_postal_code_1":"2035","comments":"","contact_phone_2":"","contact_email_2":"","contact_name_2":"","contact_phone_1":"","contact_email_1":"","contact_name_1":"","virtual_meeting_additional_info":"By phone 02 8015 6011Meeting ID: 83037287669 Passcode: 096387","root_server_uri":"http://54.153.167.239/main_server","format_shared_id_list":"14,40,54"}]';
+        Functions\when('curl_exec')->justReturn($json);
+        // redefine('time', always(strtotime('Dec 31, 1999')));
+        // echo date('Y-m-d', time());
+        global $wpdb;
+        $wpdb = Mockery::mock( 'wpdb' );
+        /** @var Mockery::mock $wpdb test */
         $wpdb->shouldReceive( 'insert' )->andReturn( array('0'=>'1') )->set('insert_id',10);
         $response = meeting_update_form_handler_rest($form_post);
          var_dump($response);
@@ -133,6 +162,7 @@ final class meeting_update_form_handlerTest extends TestCase
         $json = '[{"id_bigint":"3277","worldid_mixed":"OLM297","service_body_bigint":"6","weekday_tinyint":"3","venue_type":"2","start_time":"19:00:00","duration_time":"01:00:00","time_zone":"","formats":"JT,LC,VM","longitude":"151.2437","latitude":"-33.9495","meeting_name":"Online Meeting - Maroubra Nightly","location_text":"Online","location_info":"","location_street":"","location_neighborhood":"","location_municipality":"Maroubra","location_sub_province":"","location_province":"NSW","location_postal_code_1":"2035","comments":"","contact_phone_2":"","contact_email_2":"","contact_name_2":"","contact_phone_1":"","contact_email_1":"","contact_name_1":"","virtual_meeting_additional_info":"By phone 02 8015 6011Meeting ID: 83037287669 Passcode: 096387","root_server_uri":"http://54.153.167.239/main_server","format_shared_id_list":"14,40,54"}]';
         global $wpdb;
         $wpdb = Mockery::mock( 'wpdb' );
+        /** @var Mockery::mock $wpdb test */
         $wpdb->shouldReceive( 'insert' )->andReturn( array('0'=>'1') )->set('insert_id',10);
         $response = meeting_update_form_handler_rest($form_post);
          var_dump($response);
@@ -166,6 +196,7 @@ final class meeting_update_form_handlerTest extends TestCase
         $json = '[{"id_bigint":"3277","worldid_mixed":"OLM297","service_body_bigint":"6","weekday_tinyint":"3","venue_type":"2","start_time":"19:00:00","duration_time":"01:00:00","time_zone":"","formats":"JT,LC,VM","longitude":"151.2437","latitude":"-33.9495","meeting_name":"Online Meeting - Maroubra Nightly","location_text":"Online","location_info":"","location_street":"","location_neighborhood":"","location_municipality":"Maroubra","location_sub_province":"","location_province":"NSW","location_postal_code_1":"2035","comments":"","contact_phone_2":"","contact_email_2":"","contact_name_2":"","contact_phone_1":"","contact_email_1":"","contact_name_1":"","virtual_meeting_additional_info":"By phone 02 8015 6011Meeting ID: 83037287669 Passcode: 096387","root_server_uri":"http://54.153.167.239/main_server","format_shared_id_list":"14,40,54"}]';
         global $wpdb;
         $wpdb = Mockery::mock( 'wpdb' );
+        /** @var Mockery::mock $wpdb test */
         $wpdb->shouldReceive( 'insert' )->andReturn( array('0'=>'1') )->set('insert_id',10);
         $response = meeting_update_form_handler_rest($form_post);
          var_dump($response);
