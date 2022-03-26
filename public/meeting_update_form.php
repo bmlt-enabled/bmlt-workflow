32bb2016afe0e8ae6a39d1d682a0dc9a903c5022
Nigel <nigel.bmlt@gmail.com>
2022-03-26 11:09:39 +1100

    add email fix

--- public/meeting_update_form.php
+++ public/meeting_update_form.php
@@ -33,17 +33,17 @@ wp_nonce_field('wp_rest', '_wprestnonce');
             <div id="meeting_selector">
                 <br>
                 <label for="update_reason"">Search For Meeting:</label>
-                <select class="meeting-searcher" id="meeting-searcher">
+                <select class=" meeting-searcher" id="meeting-searcher">
                     <option></option>
-                </select>
-                <br><br>
-                <input type="hidden" name="meeting_id" id="meeting_id" value="">
+                    </select>
+                    <br><br>
+                    <input type="hidden" name="meeting_id" id="meeting_id" value="">
             </div>
             <div id="meeting_content" class="form-grid">
                 <div class="form-grid-top">
                     <p id="instructions"></p>
                 </div>
-                    <!-- personal details -->
+                <!-- personal details -->
                 <div id="personal_details" class="form-grid-col2">
                     <fieldset>
                         <legend>Personal Details</legend>
@@ -55,117 +55,118 @@ wp_nonce_field('wp_rest', '_wprestnonce');
                         <input type="email" name="email_address" id="email_address" size="50" required>
                         <label for="add_email" class="add_email">Add this email as a contact
                             address for the group</label>
-                        <div class="checkbox-group">
-                            <input name="add_email" id="add_email-0" value="yes" type="checkbox">
-                            <label for="add_email-0">Yes</label>
-                            <label for="contact_number_confidential" class="formbuilder-number-label">Contact Number (Confidential)</label>
-                            <input type="number" name="contact_number_confidential" id="contact_number_confidential">
-                            <label for="group_relationship">Are you a?</label>
-                            <select name="group_relationship" id="group_relationship">
-                                <option value="Group Member">Group Member</option>
-                                <option value="Area Trusted Servant">Area Trusted Servant</option>
-                                <option value="Regional Trusted Servant">Regional Trusted Servant</option>
-                                <option value="NA Member">NA Member</option>
-                                <option value="Not A Member">Not A Member</option>
-                            </select>
-                        </div>
-                    </fieldset>
+                        <select name="add_email" id="add_email">
+                            <option value="yes">Yes</option>
+                            <option value="no" selected>No</option>
+                        </select>
+                        <label for="contact_number_confidential" class="formbuilder-number-label">Contact Number (Confidential)</label>
+                        <input type="number" name="contact_number_confidential" id="contact_number_confidential">
+                        <label for="group_relationship">Are you a?</label>
+                        <select name="group_relationship" id="group_relationship">
+                            <option value="Group Member">Group Member</option>
+                            <option value="Area Trusted Servant">Area Trusted Servant</option>
+                            <option value="Regional Trusted Servant">Regional Trusted Servant</option>
+                            <option value="NA Member">NA Member</option>
+                            <option value="Not A Member">Not A Member</option>
+                        </select>
                 </div>
-                <!-- meeting details -->
-                <div id="meeting_details" class="form-grid-col1">
-                    <fieldset>
-                        <legend>Meeting Details</legend>
+                </fieldset>
+            </div>
+            <!-- meeting details -->
+            <div id="meeting_details" class="form-grid-col1">
+                <fieldset>
+                    <legend>Meeting Details</legend>
 
-                        <div class="form-grid-col1">
-                            <label for="meeting_name">Group Name<span class="wbw-required-field"> *</span></label>
-                            <input type="text" name="meeting_name" size="50" id="meeting_name" required>
-                                <label for="weekday_tinyint">Meeting Day:<span class="wbw-required-field"> *</span></label>
-                                <select name="weekday_tinyint" id="weekday_tinyint">
-                                    <option value=1>Sunday</option>
-                                    <option value=2>Monday</option>
-                                    <option value=3>Tuesday</option>
-                                    <option value=4>Wednesday</option>
-                                    <option value=5>Thursday</option>
-                                    <option value=6>Friday</option>
-                                    <option value=7>Saturday</option>
-                                </select>
-                                <div class="grid-flow-input">
-                                <label for="start_time">Start Time<span class="wbw-required-field"> *</span></label>
-                                <input type="time" name="start_time" size="10" id="start_time" required>
-                                <label for="duration_hours">Duration<span class="wbw-required-field"> *</span></label>
-                                <select id="duration_hours">
-                                    <option value="00">0</option>
-                                    <option value="01" selected="selected">1</option>
-                                    <option value="02">2</option>
-                                    <option value="03">3</option>
-                                    <option value="04">4</option>
-                                    <option value="05">5</option>
-                                    <option value="06">6</option>
-                                    <option value="07">7</option>
-                                    <option value="08">8</option>
-                                    <option value="09">9</option>
-                                    <option value="10">10</option>
-                                    <option value="11">11</option>
-                                    <option value="12">12</option>
-                                </select> h
-                                <select id="duration_minutes">
-                                    <option value="00" selected="selected">0</option>
-                                    <option value="05">5</option>
-                                    <option value="10">10</option>
-                                    <option value="15">15</option>
-                                    <option value="20">20</option>
-                                    <option value="25">25</option>
-                                    <option value="30">30</option>
-                                    <option value="35">35</option>
-                                    <option value="40">40</option>
-                                    <option value="45">45</option>
-                                    <option value="50">50</option>
-                                    <option value="55">55</option>
-                                </select> m
-</div>
-                                <input type="hidden" name="duration_time" size="10" id="duration_time" required>
-                            <label for="service_body_bigint">Service Committee (or Other if not known)</label>
-                            <select name="service_body_bigint" id="service_body_bigint">
-                            </select>
-                            <label for="location_text">Location (eg: a building name)<span class="wbw-required-field"> *</span></label>
-                            <input type="text" name="location_text" size="50" id="location_text" required>
-                            <label for="location_street">Street Address<span class="wbw-required-field"> *</span></label>
-                            <input type="text" name="location_street" size="50" id="location_street" required>
-                            <label for="location_info">Extra Location Info (eg: Near the park)</label>
-                            <input type="text" name="location_info" size="50" id="location_info">
-                            <label for="location_municipality">City/Town/Suburb<span class="wbw-required-field"> *</span></label>
-                            <input type="text" name="location_municipality" size="50" id="location_municipality" required>
-                            <label for="location_province">State<span class="wbw-required-field"> *</span></label>
-                            <input type="text" name="location_province" size="50" id="location_province" required>
-                            <label for="location_postal_code_1">Postcode<span class="wbw-required-field"> *</span></label>
-                            <input type="number" name="location_postal_code_1" size="5" max="9999" id="location_postal_code_1" required>
-                            
-                            <label for="meeting_update_form_format_shared_id_list">Meeting Formats</label>
-                            <select name="meeting_update_form_format_shared_id_list" id="meeting_update_form_format_shared_id_list" style="width: auto"></select>
+                    <div class="form-grid-col1">
+                        <label for="meeting_name">Group Name<span class="wbw-required-field"> *</span></label>
+                        <input type="text" name="meeting_name" size="50" id="meeting_name" required>
+                        <label for="weekday_tinyint">Meeting Day:<span class="wbw-required-field"> *</span></label>
+                        <select name="weekday_tinyint" id="weekday_tinyint">
+                            <option value=1>Sunday</option>
+                            <option value=2>Monday</option>
+                            <option value=3>Tuesday</option>
+                            <option value=4>Wednesday</option>
+                            <option value=5>Thursday</option>
+                            <option value=6>Friday</option>
+                            <option value=7>Saturday</option>
+                        </select>
+                        <div class="grid-flow-input">
+                            <label for="start_time">Start Time<span class="wbw-required-field"> *</span></label>
+                            <input type="time" name="start_time" size="10" id="start_time" required>
+                            <label for="duration_hours">Duration<span class="wbw-required-field"> *</span></label>
+                            <select id="duration_hours">
+                                <option value="00">0</option>
+                                <option value="01" selected="selected">1</option>
+                                <option value="02">2</option>
+                                <option value="03">3</option>
+                                <option value="04">4</option>
+                                <option value="05">5</option>
+                                <option value="06">6</option>
+                                <option value="07">7</option>
+                                <option value="08">8</option>
+                                <option value="09">9</option>
+                                <option value="10">10</option>
+                                <option value="11">11</option>
+                                <option value="12">12</option>
+                            </select> h
+                            <select id="duration_minutes">
+                                <option value="00" selected="selected">0</option>
+                                <option value="05">5</option>
+                                <option value="10">10</option>
+                                <option value="15">15</option>
+                                <option value="20">20</option>
+                                <option value="25">25</option>
+                                <option value="30">30</option>
+                                <option value="35">35</option>
+                                <option value="40">40</option>
+                                <option value="45">45</option>
+                                <option value="50">50</option>
+                                <option value="55">55</option>
+                            </select> m
+                        </div>
+                        <input type="hidden" name="duration_time" size="10" id="duration_time" required>
+                        <label for="service_body_bigint">Service Committee (or Other if not known)</label>
+                        <select name="service_body_bigint" id="service_body_bigint">
+                        </select>
+                        <label for="location_text">Location (eg: a building name)<span class="wbw-required-field"> *</span></label>
+                        <input type="text" name="location_text" size="50" id="location_text" required>
+                        <label for="location_street">Street Address<span class="wbw-required-field"> *</span></label>
+                        <input type="text" name="location_street" size="50" id="location_street" required>
+                        <label for="location_info">Extra Location Info (eg: Near the park)</label>
+                        <input type="text" name="location_info" size="50" id="location_info">
+                        <label for="location_municipality">City/Town/Suburb<span class="wbw-required-field"> *</span></label>
+                        <input type="text" name="location_municipality" size="50" id="location_municipality" required>
+                        <label for="location_province">State<span class="wbw-required-field"> *</span></label>
+                        <input type="text" name="location_province" size="50" id="location_province" required>
+                        <label for="location_postal_code_1">Postcode<span class="wbw-required-field"> *</span></label>
+                        <input type="number" name="location_postal_code_1" size="5" max="9999" id="location_postal_code_1" required>
 
-                            <label for=" virtual_meeting_link">Online Meeting Link</label>
-                            <input type="url" name="virtual_meeting_link" size="50" id="virtual_meeting_link">
-                        </div>
-                    </fieldset>
-                </div>
-                <!-- other details -->
-                <div class="form-grid-bottom">
-                    <fieldset>
-                        <legend>Additional Information</legend>
-                        <textarea name="additional_info" id="additional_info" rows="5" cols="50" placeholder="Provide any more detail that may help us action your meeting change request"></textarea>
-                        <div id="starter_pack">
-                            <label for="starter_kit_required">Starter Kit Required</label>
-                            <select name="starter_kit_required" id="starter_kit_required">
-                                <option value="yes" selected="true" id="starter_kit_required_yes">Yes</option>
-                                <option value="no" id="starter_kit_required_no">No</option>
-                            </select>
-                            <label for="starter_kit_postal_address">Starter Kit Postal Address<span class="wbw-required-field"> *</span></label>
-                            <textarea name="starter_kit_postal_address" id="starter_kit_postal_address" rows="5" cols="50"></textarea>
-                        </div>
-                    </fieldset>
-                    <br><input type="submit" name="submit" id="submit" class="button button-primary" value="Submit Form"></p>
-                </div>
+                        <label for="meeting_update_form_format_shared_id_list">Meeting Formats</label>
+                        <select name="format_shared_id_list" id="meeting_update_form_format_shared_id_list"></select>
+
+                        <label for=" virtual_meeting_link">Online Meeting Link</label>
+                        <input type="url" name="virtual_meeting_link" size="50" id="virtual_meeting_link">
+                    </div>
+                </fieldset>
+            </div>
+            <!-- other details -->
+            <div class="form-grid-bottom">
+                <fieldset>
+                    <legend>Additional Information</legend>
+                    <textarea name="additional_info" id="additional_info" rows="5" cols="50" placeholder="Provide any more detail that may help us action your meeting change request"></textarea>
+                    <div id="starter_pack">
+                        <label for="starter_kit_required">Starter Kit Required</label>
+                        <select name="starter_kit_required" id="starter_kit_required">
+                            <option value="yes" selected="true" id="starter_kit_required_yes">Yes</option>
+                            <option value="no" id="starter_kit_required_no">No</option>
+                        </select>
+                        <label for="starter_kit_postal_address">Starter Kit Postal Address<span class="wbw-required-field"> *</span></label>
+                        <textarea name="starter_kit_postal_address" id="starter_kit_postal_address" rows="5" cols="50"></textarea>
+                    </div>
+                </fieldset>
+                <br><input type="submit" name="submit" id="submit" class="button button-primary" value="Submit Form"></p>
             </div>
         </div>
-    </form>
+</div>
+</form>
 </div>
