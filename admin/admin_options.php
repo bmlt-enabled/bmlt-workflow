d3e1923d9402e7acbefe79c4cef3dc68f5f7357c
Nigel <nigel.bmlt@gmail.com>
2022-03-31 22:16:06 +1100

    update

--- admin/admin_options.php
+++ admin/admin_options.php
@@ -2,16 +2,10 @@
 
 if (!defined('ABSPATH')) exit; // die if being called directly
 
-$wbw_close_meeting_template_default = htmlentities(file_get_contents(WBW_PLUGIN_DIR . 'templates/default_close_meeting_email_template.html'));
-$wbw_other_meeting_template_default = htmlentities(file_get_contents(WBW_PLUGIN_DIR . 'templates/default_other_meeting_email_template.html'));
-$wbw_new_meeting_template_default = htmlentities(file_get_contents(WBW_PLUGIN_DIR . 'templates/default_new_meeting_email_template.html'));
-$wbw_existing_meeting_template_default = htmlentities(file_get_contents(WBW_PLUGIN_DIR . 'templates/default_existing_meeting_email_template.html'));
+$wbw_submitter_email_template_default = htmlentities(file_get_contents(WBW_PLUGIN_DIR . 'templates/default_submitter_email_template.html'));
 $wbw_fso_email_template_default = htmlentities(file_get_contents(WBW_PLUGIN_DIR . 'templates/default_fso_email_template.html'));
 
-echo '<div style="position:absolute; top:0; left:-500px;"><textarea rows="1" cols="2" id="wbw_new_meeting_template_default">'.$wbw_new_meeting_template_default.'</textarea></div>';
-echo '<div style="position:absolute; top:0; left:-500px;"><textarea rows="1" cols="2" id="wbw_existing_meeting_template_default">'.$wbw_existing_meeting_template_default.'</textarea></div>';
-echo '<div style="position:absolute; top:0; left:-500px;"><textarea rows="1" cols="2" id="wbw_close_meeting_template_default">'.$wbw_close_meeting_template_default.'</textarea></div>';
-echo '<div style="position:absolute; top:0; left:-500px;"><textarea rows="1" cols="2" id="wbw_other_meeting_template_default">'.$wbw_other_meeting_template_default.'</textarea></div>';
+echo '<div style="position:absolute; top:0; left:-500px;"><textarea rows="1" cols="2" id="wbw_submitter_email_template_default">'.$wbw_submitter_email_template_default.'</textarea></div>';
 echo '<div style="position:absolute; top:0; left:-500px;"><textarea rows="1" cols="2" id="wbw_fso_email_template_default">'.$wbw_fso_email_template_default.'</textarea></div>';
 
 wp_nonce_field('wp_rest', '_wprestnonce');
