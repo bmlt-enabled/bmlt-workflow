88dc7f01e9ae6966f8b9716ee31d3afdf292d5d4
Nigel <nigel.bmlt@gmail.com>
2022-04-05 11:43:51 +1000

    custom autoload

--- wordpress-bmlt-workflow.php
+++ wordpress-bmlt-workflow.php
@@ -20,6 +20,7 @@ if (file_exists('vendor/autoload.php')) {
     // custom autoloader if not. only autoloads out of src directory
 
     spl_autoload_register(function (string $class) {
+        $class = str_replace('wbw\\','', $class);
         require __DIR__ . '/src/' . str_replace('\\', '/', $class) . '.php';
     });
 }
