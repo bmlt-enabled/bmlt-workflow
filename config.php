02a8c48466494569bb1642c823109d1453679785
Nigel <nigel.bmlt@gmail.com>
2022-04-07 17:09:32 +1000

    disable debug

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-define('WBW_DEBUG', true);
+define('WBW_DEBUG', false);
 define('WBW_PLUGIN_DIR', plugin_dir_path(__FILE__));
 // placeholder for an 'other' service body
 define('CONST_OTHER_SERVICE_BODY', '99999999999');
