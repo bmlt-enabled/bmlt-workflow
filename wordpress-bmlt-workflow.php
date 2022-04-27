bb776ec6ff022fe67dde4adeface1aa32b9925f9
Nigel <nigel.bmlt@gmail.com>
2022-04-27 15:46:14 +1000

    bump version number

--- wordpress-bmlt-workflow.php
+++ wordpress-bmlt-workflow.php
@@ -4,7 +4,7 @@
  * Plugin Name: Wordpress BMLT Workflow
  * Plugin URI: https://github.com/bmlt-enabled/wordpress-bmlt-workflow
  * Description: Wordpress BMLT Workflow
- * Version: 0.3.8
+ * Version: 0.3.9
  * Author: @nigel-bmlt
  * Author URI: https://github.com/nigel-bmlt
  **/
