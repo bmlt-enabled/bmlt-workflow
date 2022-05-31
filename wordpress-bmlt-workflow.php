7defe84d21891f9c44fdd6c6b803b402c8b12221
Nigel <nigel.bmlt@gmail.com>
2022-05-31 12:48:45 +1000

    update

--- wordpress-bmlt-workflow.php
+++ wordpress-bmlt-workflow.php
@@ -224,7 +224,7 @@ function wbw_admin_scripts($hook)
 
             // inline scripts
             $script  = 'var wbw_admin_bmltserver_rest_url = ' . json_encode(get_rest_url() . $wbw_rest_namespace . '/bmltserver') . '; ';
-            $script  += 'var wbw_admin_backup_rest_url = ' . json_encode(get_rest_url() . $wbw_rest_namespace . '/options/backup') . '; ';
+            $script  .= 'var wbw_admin_backup_rest_url = ' . json_encode(get_rest_url() . $wbw_rest_namespace . '/options/backup') . '; ';
 
             wp_add_inline_script('admin_options_js', $script, 'before');
             break;
