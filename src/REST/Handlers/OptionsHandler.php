181b20f89a95708ae26d1e2004b45c1611f1891a
Nigel <nigel.bmlt@gmail.com>
2022-06-05 22:12:29 +1000

    filename

--- src/REST/Handlers/OptionsHandler.php
+++ src/REST/Handlers/OptionsHandler.php
@@ -56,8 +56,7 @@ class OptionsHandler
         $wbw_dbg->debug_log(json_encode($save, JSON_PRETTY_PRINT));
         mkdir(WBW_PLUGIN_DIR."/backups",0700);
         $dateTime = new \DateTime();
-        $dateTime->format(\DateTimeInterface::RFC3339_EXTENDED);
-        $fname = $dateTime.".json";
+        $fname = $dateTime->format(\DateTimeInterface::RFC3339_EXTENDED).".json";
         $wbw_dbg->debug_log("filename = ".$fname);
         return $this->handlerCore->wbw_rest_success('Backup completed.');
 
