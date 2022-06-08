92c1000263592dd91858e00ea5b9aaa126ab0c66
Nigel <nigel.bmlt@gmail.com>
2022-06-08 15:24:29 +1000

    banner

--- config.php
+++ config.php
@@ -1,3 +1,5 @@
 <?php
 define('WBW_DEBUG', false);
 define('WBW_PLUGIN_DIR', plugin_dir_path(__FILE__));
+define('WBW_PLUGIN_URL', plugin_dir_url(__FILE__));
+
--- config_phpunit.php
+++ config_phpunit.php
@@ -1,5 +1,7 @@
 <?php
 define('WBW_DEBUG', false);
+define('WBW_PLUGIN_DIR', plugin_dir_path(__FILE__));
+define('WBW_PLUGIN_URL', plugin_dir_url(__FILE__));
 
 // get us through the header
 if (!defined('ABSPATH')) {
