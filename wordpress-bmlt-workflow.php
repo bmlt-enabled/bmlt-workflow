fcba1a57747234b9cd90a22ceb5b98608a6010d7
Nigel <nigel.bmlt@gmail.com>
2022-06-14 23:24:09 +1000

    update

--- wordpress-bmlt-workflow.php
+++ wordpress-bmlt-workflow.php
@@ -193,7 +193,7 @@ if (!class_exists('wbw_plugin')) {
             $this->prevent_cache_register_script('wbw-general-js', array('jquery'), 'js/script_includes.js');
             $this->prevent_cache_register_script('wbw-meeting-update-form-js', array('jquery', 'jquery.validate'), 'js/meeting_update_form.js');
             $this->prevent_cache_register_style('wbw-meeting-update-form-css', false, 'css/meeting_update_form.css');
-            wp_register_script('jquery.validate', 'https://cdn.jsdelivr.net/npm/jquery-validation@1.19.3/dist/jquery.validate.min.js', array('jquery'), '1.0', true);
+            wp_register_script('jquery-validate', 'https://cdn.jsdelivr.net/npm/jquery-validation@1.19.3/dist/jquery.validate.min.js', array('jquery'), '1.0', true);
             wp_register_script('jquery.validate.additional', 'https://cdn.jsdelivr.net/npm/jquery-validation@1.19.3/dist/additional-methods.min.js', array('jquery', 'jquery.validate'), '1.0', true);
             wp_enqueue_style('dashicons');
             $this->debug_log("scripts and styles registered");
