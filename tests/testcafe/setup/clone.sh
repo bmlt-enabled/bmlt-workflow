c63e719f3d3869d442d7548d1d64e4ca6a0bb961
Nigel <nigel.bmlt@gmail.com>
2022-06-16 21:05:30 +1000

    update

--- tests/testcafe/setup/clone.sh
+++ tests/testcafe/setup/clone.sh
@@ -1,7 +1,11 @@
 # git clone https://github.com/bmlt-enabled/wordpress-bmlt-workflow.git
+#
 cd /home/ssm-user
 cd wordpress-bmlt-workflow
 git pull
+cp tests/testcafe/setup/github/* /var/www/html/github
+cp tests/testcafe/setup/*.sh /home/ssm-user/scripts
+cp tests/testcafe/setup/*.sql /home/ssm-user/scripts
 cd ..
 rm -rf /home/ssm-user/wbw/*
 cp -R wordpress-bmlt-workflow/* wbw
