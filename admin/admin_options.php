454793d1381f574722f5fd60c2d71997f05724ef
Nigel <nigel.bmlt@gmail.com>
2022-06-19 15:09:42 +1000

    update

--- admin/admin_options.php
+++ admin/admin_options.php
@@ -10,8 +10,6 @@ $WBW_WP_Options = new WBW_WP_Options();
 $wbw_submitter_email_template_default = htmlentities(file_get_contents(WBW_PLUGIN_DIR . 'templates/default_submitter_email_template.html'));
 $wbw_fso_email_template_default = htmlentities(file_get_contents(WBW_PLUGIN_DIR . 'templates/default_fso_email_template.html'));
 
-settings_errors();
-
 echo '<div style="position:absolute; top:0; left:-500px;"><textarea rows="1" cols="2" id="wbw_submitter_email_template_default">' . $wbw_submitter_email_template_default . '</textarea></div>';
 echo '<div style="position:absolute; top:0; left:-500px;"><textarea rows="1" cols="2" id="wbw_fso_email_template_default">' . $wbw_fso_email_template_default . '</textarea></div>';
 echo '<div class="wbw_banner"></div>';
@@ -20,6 +18,8 @@ wp_nonce_field('wp_rest', '_wprestnonce');
 echo '<hr class="wbw-error-message">';
 echo '<div class="wrap">';
 echo '<form method="post" action="options.php">';
+settings_errors();
+
 settings_fields('wbw-settings-group');
 do_settings_sections('wbw-settings');
 
