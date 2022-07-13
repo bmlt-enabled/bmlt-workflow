638bfc96786e8f544c2edae3ac7e0ed27c268478
Nigel <nigel.bmlt@gmail.com>
2022-07-13 22:15:01 +0930

    fix uninstaller

--- uninstall.php
+++ uninstall.php
@@ -4,23 +4,26 @@ use wbw\WBW_WP_Options;
 use wbw\WBW_Debug;
 use wbw\WBW_Database;
 
-foreach ($this->WBW_WP_Options->wbw_options as $key => $value) {
-    $this->debug_log("deleting option " . $value);
+$WBW_WP_Options = new WBW_WP_Options();
+$WBW_Database = new WBW_Database();
+
+foreach ($WBW_WP_Options->wbw_options as $key => $value) {
+    error_log("deleting option " . $value);
     delete_option($value);
 }
 
-$this->WBW_Database->wbw_drop_tables();
-$this->debug_log("removed tables");
+$WBW_Database->wbw_drop_tables();
+error_log("removed tables");
 
 // remove custom capability
 
-$this->debug_log("deleting capabilities");
+error_log("deleting capabilities");
 
 $users = get_users();
 foreach ($users as $user) {
-    $user->remove_cap($this->WBW_WP_Options->wbw_capability_manage_submissions);
+    $user->remove_cap($WBW_WP_Options->wbw_capability_manage_submissions);
 }
 
 remove_role('wbw_trusted_servant');
 
-$this->debug_log("uninstall complete");
+error_log("uninstall complete");
