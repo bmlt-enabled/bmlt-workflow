e61e45ffac18c902e816cc5c736c790c6de5a564
Nigel <nigel.bmlt@gmail.com>
2022-07-15 21:26:32 +0930

    license headers

--- tests/testcafe/setup/bmlt_states_on.sh
+++ tests/testcafe/setup/bmlt_states_on.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
+# Copyright (C) 2022 nigel.bmlt@gmail.com
+# 
+# This file is part of bmlt-workflow.
+# 
+# bmlt-workflow is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# bmlt-workflow is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with bmlt-workflow.  If not, see <http://www.gnu.org/licenses/>.
+
 
 sed 's/%MEETING_STATES%/\$meeting_states_and_provinces = array("NSW","SA","VIC");/g' /home/ssm-user/scripts/auto-config.inc.php.in > /var/www/html/blank_bmlt/auto-config.inc.php
--- wordpress-bmlt-workflow.php
+++ wordpress-bmlt-workflow.php
@@ -1,7 +1,24 @@
 <?php
+// Copyright (C) 2022 nigel.bmlt@gmail.com
+// 
+// This file is part of bmlt-workflow.
+// 
+// bmlt-workflow is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// bmlt-workflow is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with bmlt-workflow.  If not, see <http://www.gnu.org/licenses/>.
+
 
 /**
- * Plugin Name: Wordpress BMLT Workflow
+ * Plugin Name: BMLT Workflow
  * Plugin URI: https://github.com/bmlt-enabled/wordpress-bmlt-workflow
  * Description: Workflows for BMLT meeting management!
  * Version: 0.4.4
@@ -10,6 +27,7 @@
  * Author: @nigel-bmlt
  * Author URI: https://github.com/nigel-bmlt
  **/
+
 
  define('WBW_PLUGIN_VERSION','0.4.4');
 
