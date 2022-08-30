d344e8825ca3910df8edd9bae9f24fbbacc7d42e
Nigel <nigel.bmlt@gmail.com>
2022-08-30 22:33:02 +1000

    update installer

--- tests/testcafe/setup/wp-install.sh
+++ tests/testcafe/setup/wp-install.sh
@@ -43,7 +43,7 @@ $MYSQL -e "CREATE DATABASE IF NOT EXISTS $mysqldb; GRANT ALL ON $mysqldb.* TO '$
 
 wp core download --path=$sitelocalpath
 wp config create --path=$sitelocalpath --dbname=$mysqldb --dbuser=$mysqluser --dbpass=$mysqlpass
-wp core install --title="hi" --admin_user=$wpuser --admin_password=$wppass --admin_email=$wpemail --path=$sitelocalpath
+wp core install --url=$siteurl --title="hi" --admin_user=$wpuser --admin_password=$wppass --admin_email=$wpemail --path=$sitelocalpath
 
 cd $sitelocalpath
 
