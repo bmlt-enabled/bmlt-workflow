118b6ea2528041eefa68a2c8f5ea3fe6794e9880
Nigel <nigel.bmlt@gmail.com>
2022-10-03 21:16:52 +1100

    update

--- src/BMLT/Integration.php
+++ src/BMLT/Integration.php
@@ -473,15 +473,21 @@ class Integration
 
     private function set_args($cookies, $body = null)
     {
-        $args = array(
-            'timeout' => '120',
-            'headers' => array(
-                'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36'
-            ),
-            'cookies' => isset($cookies) ? $cookies : null,
-            'body' => isset($body) ? $body : null
+        {
 
-        );
+            $args = array(
+                'timeout' => '120',
+                'headers' => array(
+                    'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36'
+                ),
+                'cookies' => isset($cookies) ? $cookies : null,
+                'body' => isset($body) ? $body : null
+
+            );
+        }
+        $this->debug_log("set_args:");
+        $this->debug_log($args);
+
         return $args;
     }
 
