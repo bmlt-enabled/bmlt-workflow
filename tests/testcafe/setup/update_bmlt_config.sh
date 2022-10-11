a24c5f3992a1acca52c800d003b31f6d7e6bb4f1
Nigel <nigel.bmlt@gmail.com>
2022-10-11 22:06:43 +1100

    update

--- tests/testcafe/setup/update_bmlt_config.sh
+++ tests/testcafe/setup/update_bmlt_config.sh
@@ -28,7 +28,7 @@ else
   AG="\$auto_geocoding_enabled = true;"
 fi
 
-GK=$(aws ssm get-parameter --name bmltwf_gmaps_key--region ap-southeast-2 --with-decryption | jq .Parameter.Value -r)
+GK=$(aws ssm get-parameter --name bmltwf_gmaps_key --region ap-southeast-2 --with-decryption | jq .Parameter.Value -r)
 
 DB=$(aws ssm get-parameter --name bmltwf_bmlt_db_password --region ap-southeast-2 --with-decryption | jq .Parameter.Value -r)
 
