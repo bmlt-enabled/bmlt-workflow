a85028b11e380a0a31bd5561cfec334c2bffaea0
Nigel <nigel.bmlt@gmail.com>
2022-11-06 22:11:06 +1100

    update installer

--- tests/testcafe/setup/wp-install.sh
+++ tests/testcafe/setup/wp-install.sh
@@ -41,6 +41,9 @@ export wp_nopriv_user=aa$(((RANDOM<<15|$RANDOM)<<15|$RANDOM))
 aws ssm put-parameter --overwrite --name bmltwf_test_wp_nopriv_user_single --value $wp_nopriv_user --type SecureString --region ap-southeast-2
 export wp_nopriv_pass=$(((RANDOM<<15|$RANDOM)<<15|$RANDOM))
 aws ssm put-parameter --overwrite --name bmltwf_test_wp_nopriv_pass_single --value $wp_nopriv_pass --type SecureString --region ap-southeast-2
+
+# single site
+
 export wpemail=nigel.bmlt@gmail.com
 export sitename=wordpresssingle
 export siteurl=http://$MYIP/$sitename
@@ -101,6 +104,85 @@ cd $sitelocalpath/wp-content/plugins/bmlt-workflow
 # activate plugin
 wp plugin activate --path=$sitelocalpath "bmlt-workflow"
 wp option --path=$sitelocalpath add 'bmltwf_bmlt_server_address' 'http://'$MYIP'/blank_bmlt/main_server/'
+wp option --path=$sitelocalpath add 'bmltwf_bmlt_username' 'bmlt-workflow-bot'
+wp option --path=$sitelocalpath add 'bmltwf_bmlt_test_status' 'success'
+wp option --path=$sitelocalpath add 'bmltwf_bmlt_password' '{"config":{"size":"MzI=","salt":"\/5ObzNuYZ\/Y5aoYTsr0sZw==","limit_ops":"OA==","limit_mem":"NTM2ODcwOTEy","alg":"Mg==","nonce":"VukDVzDkAaex\/jfB"},"encrypted":"fertj+qRqQrs9tC+Cc32GrXGImHMfiLyAW7sV6Xojw=="}' --format=json
+# create our test page
+wp post create --path=$sitelocalpath --post_type=page --post_title='testpage' --post_content='[bmltwf-meeting-update-form]' --post_status='publish' --post_name='testpage'
+# create our test users
+wp user create --path=$sitelocalpath $wp_submission_user aa123@a.com --user_pass=$wp_submission_pass
+wp user create --path=$sitelocalpath $wp_nopriv_user aa456@a.com --user_pass=$wp_nopriv_pass
+
+# single site bmlt3x
+
+export mysqlhost=localhost
+export mysqldb=wpsinglebmlt3x
+export mysqluser=wpsinglebmlt3x
+export mysqlpass=$(((RANDOM<<15|$RANDOM)<<15|$RANDOM))
+export wptitle=wpsinglebmlt3x
+export wpuser=$(((RANDOM<<15|$RANDOM)<<15|$RANDOM))
+aws ssm put-parameter --overwrite --name bmltwf_test_wpuser_multisingle --value $wpuser --type SecureString --region ap-southeast-2
+export wppass=$(((RANDOM<<15|$RANDOM)<<15|$RANDOM))
+aws ssm put-parameter --overwrite --name bmltwf_test_wppass_multisingle --value $wppass --type SecureString --region ap-southeast-2
+export wpemail=nigel.bmlt@gmail.com
+export sitename=wpsinglebmlt3x
+export siteurl=http://$MYIP/$sitename/
+export sitelocalpath=/var/www/html/$sitename
+
+sudo rm -rf $sitelocalpath
+$MYSQL -e "DROP DATABASE $mysqldb"
+# Setup DB & DB User
+$MYSQL -e "CREATE DATABASE IF NOT EXISTS $mysqldb; GRANT ALL ON $mysqldb.* TO '$mysqluser'@'$mysqlhost' IDENTIFIED BY '$mysqlpass'; FLUSH PRIVILEGES "
+
+sudo mkdir $sitelocalpath
+sudo chown ssm-user:ssm-user $sitelocalpath
+
+wp core download --path=$sitelocalpath
+wp config create --path=$sitelocalpath --dbname=$mysqldb --dbuser=$mysqluser --dbpass=$mysqlpass
+wp core install --url=$siteurl --title="hi" --admin_user=$wpuser --admin_password=$wppass --admin_email=$wpemail --path=$sitelocalpath
+
+cd $sitelocalpath
+
+# Build our wp-config.php file
+cat > insert << EOF
+// Enable WP_DEBUG mode
+define( 'WP_DEBUG', false );
+
+// Disable display of errors and warnings
+define( 'WP_DEBUG_DISPLAY', true );
+define( 'WP_DEBUG_LOG', '/home/ssm-user/php-errors.log' );
+
+// Use dev versions of core JS and CSS files (only needed if you are modifying these core files)
+define( 'SCRIPT_DEBUG', true );
+@ini_set('log_errors','On'); // enable or disable php error logging (use 'On' or 'Off')
+@ini_set('display_errors','On'); // enable or disable public display of errors (use 'On' or 'Off')
+@ini_set('mail.log','/home/ssm-user/mail.log'); // path to server-writable log file
+@ini_set('sendmail_path','/home/ssm-user/maillog.py'); // path to server-writable log file
+
+EOF
+sed -i -e '/\/\* Add any custom values between this line and the "stop editing" line. \*\//r./insert' wp-config.php
+rm insert
+sed -i -e "/define( 'WP_DEBUG', false );/d" wp-config.php
+
+# Grab our Salt Keys
+sed -i -e "s/.*NONCE_SALT.*/define('NONCE_SALT',       '4hJ:ZRFUAdfFEBq=z\$9+]Bk|\!1y8V,h#w4aNGy~o7u|BBR;u(ASi],u[Cp46qRQa');/" wp-config.php
+
+sudo chown -R apache:apache $sitelocalpath
+
+# install our plugin
+cd /home/ssm-user/wordpress
+git clone https://github.com/bmlt-enabled/bmlt-workflow.git
+cd bmlt-workflow
+git switch $BRANCH
+sed -i "s/define('BMLTWF_DEBUG', false);/define('BMLTWF_DEBUG', true);/g" config.php
+cd ..
+sudo mv bmlt-workflow $sitelocalpath/wp-content/plugins
+sudo chown -R apache:apache $sitelocalpath/wp-content/plugins/bmlt-workflow
+cd $sitelocalpath/wp-content/plugins/bmlt-workflow
+
+# activate plugin
+wp plugin activate --path=$sitelocalpath "bmlt-workflow"
+wp option --path=$sitelocalpath add 'bmltwf_bmlt_server_address' 'http://'$MYIP':8000/main_server/'
 wp option --path=$sitelocalpath add 'bmltwf_bmlt_username' 'bmlt-workflow-bot'
 wp option --path=$sitelocalpath add 'bmltwf_bmlt_test_status' 'success'
 wp option --path=$sitelocalpath add 'bmltwf_bmlt_password' '{"config":{"size":"MzI=","salt":"\/5ObzNuYZ\/Y5aoYTsr0sZw==","limit_ops":"OA==","limit_mem":"NTM2ODcwOTEy","alg":"Mg==","nonce":"VukDVzDkAaex\/jfB"},"encrypted":"fertj+qRqQrs9tC+Cc32GrXGImHMfiLyAW7sV6Xojw=="}' --format=json
