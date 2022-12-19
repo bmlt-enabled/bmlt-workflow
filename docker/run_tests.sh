baa05469eedf63e1b9cbd89b6532785a47c655c2
Nigel <nigel.bmlt@gmail.com>
2022-12-19 22:00:21 +1000

    update testing

--- docker/run_tests.sh
+++ docker/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 docker compose down
-docker compose -f ../../bmlt2x/docker/docker-compose.yml up &
-docker compose -f ../../bmlt3x/docker/docker-compose.yml up &
-docker compose up
+docker compose -f ../../bmlt2x/docker/docker-compose.yml up --detach
+docker compose -f ../../bmlt3x/docker/docker-compose.yml up --detach
+docker compose up --detach
