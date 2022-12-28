a48ddc67c48094058d158f14c5adda51420b1bda
Nigel <nigel.bmlt@gmail.com>
2022-12-28 17:41:06 +1000

    bump versions

--- bmlt-workflow.php
+++ bmlt-workflow.php
@@ -20,14 +20,14 @@
  * Plugin Name: BMLT Workflow
  * Plugin URI: https://github.com/bmlt-enabled/bmlt-workflow
  * Description: Workflows for BMLT meeting management!
- * Version: 1.0.10
+ * Version: 1.0.11
  * Requires at least: 5.2
  * Tested up to: 6.1.1
  * Author: @nigel-bmlt
  * Author URI: https://github.com/nigel-bmlt
  **/
 
-define('BMLTWF_PLUGIN_VERSION', '1.0.10');
+define('BMLTWF_PLUGIN_VERSION', '1.0.11');
 
 if ((!defined('ABSPATH') && (!defined('BMLTWF_RUNNING_UNDER_PHPUNIT')))) exit; // die if being called directly
 
