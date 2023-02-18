27588e2480114fab585b86c04a2b1e5dc5f3a125
Nigel <nigel.bmlt@gmail.com>
2023-02-18 19:15:29 +1030

    release script

---
+++ support/release.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+
+# Copyright (C) 2023 nigel.bmlt@gmail.com
+# 
+# This file is part of bmlt-workflow.
+# 
+# bmlt-workflow is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# bmlt-workflow is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with bmlt-workflow.  If not, see <http://www.gnu.org/licenses/>.
+
+
+BRANCH=$1
+RELEASE=$2
+
+if [ -z "$BRANCH" -o -z "$RELEASE" ]
+then
+    echo "$0 <branch name> <release number>"
+    exit 1
+fi
+
+if [ ! -f config.php ] && [ ! -f bmlt-workflow.php ]
+then
+    echo config.php and bmlt-workflow.php not found
+    exit 1
+fi
+
+BV=$(grep " * Version:" bmlt-workflow.php | awk '{print $3}')
+PV=$(grep "define('BMLTWF_PLUGIN_VERSION" bmlt-workflow.php | awk -F \' '{print $4}')
+RV=$(grep "Stable tag:" readme.txt | awk '{print $3}')
+
+if [ ! $PV == $RELEASE ]
+then
+    echo "BMLTWF_PLUGIN_VERSION version $PV not equal to release version $RELEASE"
+    exit 1
+fi
+
+if [ ! $BV == $RELEASE ]
+then
+    echo "BMLT Workflow header version $BV not equal to release version $RELEASE"
+    exit 1
+fi
+
+if [ ! $RV == $RELEASE ]
+then
+    echo "Readme version $RV not equal to release version $RELEASE"
+    exit 1
+fi
+
+sed -i'.bak' "s/define('BMLTWF_DEBUG', true);/define('BMLTWF_DEBUG', false);/g" ./config.php
+rm config.php.bak
+
+git switch $BRANCH
+if [ $? != 0 ]
+then
+    echo "Error locating source branch $BRANCH"
+    exit 1
+fi
+
+export DOIT='n'
+echo -n "Are you ok to merge $BRANCH into main as release $RELEASE? [yN]"
+read DOIT
+
+if [ a${DOIT}a != "aa" ] && [ $DOIT == 'y' ]
+then
+    git switch main
+    git squash --merge "$BRANCH"
+    git commit -m "$RELEASE"
+    git push
+    git tag "$RELEASE"
+    git push --tag
+else
+    exit 1
+fi
