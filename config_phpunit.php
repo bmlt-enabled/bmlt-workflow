4723349c4b42c66d85ac6cc0bbb2ffe748f29000
Nigel <nigel.bmlt@gmail.com>
2024-07-28 22:43:27 +1000

    1.1.12

--- config_phpunit.php
+++ config_phpunit.php
@@ -1,5 +1,5 @@
 <?php
-define('BMLTWF_DEBUG', true);
+define('BMLTWF_DEBUG', false);
 // required for secrets test cases
 define('BMLTWF_PHPUNIT_NONCE_SALT', '4hJ:ZRFUAdfFEBq=z$9+]Bk|!1y8V,h#w4aNGy~o7u|BBR;u(ASi],u[Cp46qRQa');
 // get us through the header
